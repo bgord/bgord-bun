ddd9fbf29baf6b7808ad9ec72f8b1109fd575920
Bartosz Gordon <gordonbartosz@gmail.com>
2024-10-16 16:45:47 +0200

    step: Add cache static files middleware

---
+++ src/cache-static-files.ts
@@ -0,0 +1,34 @@
+import * as bg from "@bgord/node";
+import { createMiddleware } from "hono/factory";
+
+export enum CacheStaticFilesStrategy {
+  never = "never",
+  always = "always",
+  five_minutes = "five_minutes",
+}
+
+export class CacheStaticFiles {
+  static handle(strategy: CacheStaticFilesStrategy) {
+    return createMiddleware(async (c, next) => {
+      if (strategy === CacheStaticFilesStrategy.never) {
+        c.res.headers.set(
+          "cache-control",
+          "private, no-cache, no-store, must-revalidate"
+        );
+      }
+      if (strategy === CacheStaticFilesStrategy.always) {
+        c.res.headers.set(
+          "cache-control",
+          `public, max-age=${bg.Time.Days(365).seconds}, immutable`
+        );
+      }
+      if (strategy === CacheStaticFilesStrategy.five_minutes) {
+        c.res.headers.set(
+          "cache-control",
+          `public, max-age=${bg.Time.Minutes(5).seconds}`
+        );
+      }
+      return next();
+    });
+  }
+}
