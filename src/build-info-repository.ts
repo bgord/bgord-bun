af51c5c1b7a13a26094822a1053f29030f1be611
Bartosz Gordon <gordonbartosz@gmail.com>
2025-05-31 17:45:45 +0200

    refactor: migrate build-info-repository

---
+++ src/build-info-repository.ts
@@ -0,0 +1,27 @@
+import { z } from "zod/v4";
+
+export const BuildVersionSchema = z.string().min(1).max(128);
+export type BuildVersionSchemaType = z.infer<typeof BuildVersionSchema>;
+
+export type BuildInfoType = {
+  // TODO: previously Schema.TimestampType
+  BUILD_DATE: number;
+  BUILD_VERSION?: BuildVersionSchemaType;
+};
+
+// TODO: add test
+export class BuildInfoRepository {
+  static async extract(): Promise<BuildInfoType> {
+    const BUILD_DATE = Date.now();
+
+    try {
+      const packageJson = await Bun.file("package.json").json();
+
+      const BUILD_VERSION = BuildVersionSchema.parse(packageJson.version);
+
+      return { BUILD_DATE, BUILD_VERSION };
+    } catch (error) {
+      return { BUILD_DATE };
+    }
+  }
+}
