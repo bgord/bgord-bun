26ed35631ea56aef4c7afb576e82d3d0e373a64e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-01 16:25:07 +0200

    refactor: move api key schema to tools

--- src/api-key-shield.ts
+++ src/api-key-shield.ts
@@ -1,11 +1,8 @@
+import * as tools from "@bgord/tools";
 import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
-import { z } from "zod/v4";
 
-export const ApiKeySchema = z.string().trim().length(64);
-export type ApiKeySchemaType = z.infer<typeof ApiKeySchema>;
-
-type ApiKeyShieldConfigType = { API_KEY: ApiKeySchemaType };
+type ApiKeyShieldConfigType = { API_KEY: tools.ApiKeyType };
 
 export const AccessDeniedApiKeyError = new HTTPException(403, {
   message: "access_denied_api_key",
