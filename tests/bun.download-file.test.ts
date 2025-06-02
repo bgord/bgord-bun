d77af677f3cbc168e811a7d93c550d6bb98eef5f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-02 21:37:51 +0200

    step: test download file

---
+++ tests/bun.download-file.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "bun:test";
+import * as tools from "@bgord/tools";
+import { DownloadFile } from "../src/download-file";
+
+test("DownloadFile.attach returns correct headers", () => {
+  const config = {
+    filename: "example.txt",
+    mime: new tools.Mime("text/plain"),
+  };
+
+  const response = DownloadFile.attach(config);
+
+  expect(response.headers).toBeInstanceOf(Headers);
+  expect(response.headers.get("Content-Disposition")).toBe('attachment; filename="example.txt"');
+  expect(response.headers.get("Content-Type")).toBe("text/plain");
+});
