7eddfa1cc607f1fdefd6d5efc5634a8b483232fe
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-02 16:20:02 +0200

    step: test memory consumption

---
+++ tests/bun.memory-consumption.test.ts
@@ -0,0 +1,20 @@
+import { expect, spyOn, test } from "bun:test";
+import * as tools from "@bgord/tools";
+import { MemoryConsumption } from "../src/memory-consumption";
+
+test("MemoryConsumption.get returns current RSS memory as Size", () => {
+  const fakeRss = 123456789;
+
+  const memoryUsageMock = spyOn(process, "memoryUsage").mockImplementation(
+    // @ts-expect-error
+    () => ({ rss: fakeRss }),
+  );
+
+  const result = MemoryConsumption.get();
+
+  expect(result).toBeInstanceOf(tools.Size);
+  expect(result.toBytes()).toBe(fakeRss);
+  expect(result.format(tools.SizeUnit.MB)).toBe("117.74 MB");
+
+  memoryUsageMock.mockRestore();
+});
