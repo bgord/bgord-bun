f7be7c899953a091d13863e2443e7406184315c3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-03 00:29:42 +0200

    step: test graceful shutdown

---
+++ tests/bun.graceful-shutdown.test.ts
@@ -0,0 +1,63 @@
+import { afterEach, beforeEach, expect, jest, test } from "bun:test";
+import { GracefulShutdown } from "../src/graceful-shutdown";
+
+beforeEach(() => {
+  // @ts-expect-error
+  process.exit = jest.fn();
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+test("gracefully shuts down on SIGINT", async () => {
+  const stop = jest.fn();
+  const callback = jest.fn();
+
+  const mockServer = { stop };
+
+  // @ts-expect-error
+  GracefulShutdown.applyTo(mockServer, callback);
+
+  try {
+    process.emit("SIGINT");
+  } catch (_) {}
+
+  expect(stop).toHaveBeenCalled();
+  expect(callback).toHaveBeenCalled();
+});
+
+test("gracefully shuts down on SIGTERM", async () => {
+  const stop = jest.fn();
+  const callback = jest.fn();
+
+  const mockServer = { stop };
+
+  // @ts-expect-error
+  GracefulShutdown.applyTo(mockServer, callback);
+
+  try {
+    process.emit("SIGTERM");
+  } catch (_) {}
+
+  expect(stop).toHaveBeenCalled();
+  expect(callback).toHaveBeenCalled();
+});
+
+test("handles unhandledRejection and exits with code 1", async () => {
+  const stop = jest.fn();
+  const callback = jest.fn();
+
+  const mockServer = { stop };
+
+  //@ts-expect-error
+  GracefulShutdown.applyTo(mockServer, callback);
+
+  try {
+    //@ts-expect-error
+    process.emit("unhandledRejection", new Error("oops"));
+  } catch (_) {}
+
+  expect(stop).toHaveBeenCalled();
+  expect(callback).toHaveBeenCalled();
+});
