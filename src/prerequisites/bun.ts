6aad64eefddeebba7300a13540a2e5e4b3e46212
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-03 23:19:52 +0200

    step: migrate bun prereq

---
+++ src/prerequisites/bun.ts
@@ -0,0 +1,32 @@
+import * as tools from "@bgord/tools";
+
+import {
+  AbstractPrerequisite,
+  PrerequisiteLabelType,
+  PrerequisiteStatusEnum,
+  PrerequisiteStrategyEnum,
+} from "../prerequisites";
+
+export type PrerequisiteBunConfigType = {
+  version: tools.PackageVersion;
+  label: PrerequisiteLabelType;
+  enabled?: boolean;
+  current: string;
+};
+
+export class PrerequisiteBun extends AbstractPrerequisite<PrerequisiteBunConfigType> {
+  readonly strategy = PrerequisiteStrategyEnum.bun;
+
+  constructor(readonly config: PrerequisiteBunConfigType) {
+    super(config);
+  }
+
+  async verify(): Promise<PrerequisiteStatusEnum> {
+    const current = tools.PackageVersion.fromString(this.config.current);
+
+    if (current.isGreaterThanOrEqual(this.config.version)) {
+      return this.pass();
+    }
+    return this.reject();
+  }
+}
---
+++ tests/bun.bun.test.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "bun:test";
+import * as tools from "@bgord/tools";
+import { PrerequisiteStatusEnum } from "../src/prerequisites";
+import { PrerequisiteBun } from "../src/prerequisites/bun";
+
+test("returns success if current Bun version is equal to required version", async () => {
+  const prerequisite = new PrerequisiteBun({
+    label: "Bun Version Check",
+    version: tools.PackageVersion.fromString("1.0.0"),
+    current: "1.0.0",
+  });
+
+  const result = await prerequisite.verify();
+
+  expect(result).toBe(PrerequisiteStatusEnum.success);
+  expect(prerequisite.status).toBe(PrerequisiteStatusEnum.success);
+});
+
+test("returns success if current Bun version is greater than required version", async () => {
+  const prerequisite = new PrerequisiteBun({
+    label: "Bun Version Check",
+    version: tools.PackageVersion.fromString("1.0.0"),
+    current: "1.1.0",
+  });
+
+  const result = await prerequisite.verify();
+
+  expect(result).toBe(PrerequisiteStatusEnum.success);
+  expect(prerequisite.status).toBe(PrerequisiteStatusEnum.success);
+});
+
+test("returns failure if current Bun version is less than required version", async () => {
+  const prerequisite = new PrerequisiteBun({
+    label: "Bun Version Check",
+    version: tools.PackageVersion.fromString("1.2.0"),
+    current: "1.1.0",
+  });
+
+  const result = await prerequisite.verify();
+
+  expect(result).toBe(PrerequisiteStatusEnum.failure);
+  expect(prerequisite.status).toBe(PrerequisiteStatusEnum.failure);
+});
