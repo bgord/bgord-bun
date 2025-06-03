2a67e0781ff609bf4bae98dfb7cea42632d3931f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-03 23:29:03 +0200

    step: migrate timezone utc prereq

---
+++ src/prerequisites/timezone-utc.ts
@@ -0,0 +1,34 @@
+import * as tools from "@bgord/tools";
+import { z } from "zod/v4";
+
+import {
+  AbstractPrerequisite,
+  PrerequisiteLabelType,
+  PrerequisiteStatusEnum,
+  PrerequisiteStrategyEnum,
+} from "../prerequisites";
+
+export type PrerequisiteTimezoneUtcConfigType = {
+  timezone: tools.TimezoneType;
+  label: PrerequisiteLabelType;
+  enabled?: boolean;
+};
+
+export class PrerequisiteTimezoneUTC extends AbstractPrerequisite<PrerequisiteTimezoneUtcConfigType> {
+  readonly strategy = PrerequisiteStrategyEnum.timezoneUTC;
+
+  constructor(readonly config: PrerequisiteTimezoneUtcConfigType) {
+    super(config);
+  }
+
+  async verify(): Promise<PrerequisiteStatusEnum> {
+    if (!this.enabled) return PrerequisiteStatusEnum.undetermined;
+
+    try {
+      z.literal("UTC").parse(this.config.timezone);
+      return this.pass();
+    } catch (error) {
+      return this.reject();
+    }
+  }
+}
---
+++ tests/bun.timezone-utc.test.ts
@@ -0,0 +1,40 @@
+import { expect, test } from "bun:test";
+import { PrerequisiteStatusEnum } from "../src/prerequisites";
+import { PrerequisiteTimezoneUTC } from "../src/prerequisites/timezone-utc";
+
+test("returns success if timezone is valid UTC", async () => {
+  const prerequisite = new PrerequisiteTimezoneUTC({
+    label: "Timezone Check",
+    timezone: "UTC",
+  });
+
+  const result = await prerequisite.verify();
+
+  expect(result).toBe(PrerequisiteStatusEnum.success);
+  expect(prerequisite.status).toBe(PrerequisiteStatusEnum.success);
+});
+
+test("returns failure if timezone is invalid", async () => {
+  const prerequisite = new PrerequisiteTimezoneUTC({
+    label: "Timezone Check",
+    timezone: "Europe/Warsaw",
+  });
+
+  const result = await prerequisite.verify();
+
+  expect(result).toBe(PrerequisiteStatusEnum.failure);
+  expect(prerequisite.status).toBe(PrerequisiteStatusEnum.failure);
+});
+
+test("returns undetermined if disabled", async () => {
+  const prerequisite = new PrerequisiteTimezoneUTC({
+    label: "Timezone Check",
+    timezone: "UTC",
+    enabled: false,
+  });
+
+  const result = await prerequisite.verify();
+
+  expect(result).toBe(PrerequisiteStatusEnum.undetermined);
+  expect(prerequisite.status).toBe(PrerequisiteStatusEnum.undetermined);
+});
