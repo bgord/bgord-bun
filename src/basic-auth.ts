515373f539178b4af4a722002f58f0bd98fc5213
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-07 00:06:30 +0200

    feature: add basic auth headers

--- src/basic-auth.ts
+++ src/basic-auth.ts
@@ -7,3 +7,20 @@ export type BasicAuthUsernameType = z.infer<typeof BasicAuthUsername>;
 export const BasicAuthPassword = z.string().min(1).max(128);
 
 export type BasicAuthPasswordType = z.infer<typeof BasicAuthPassword>;
+
+type BasicAuthHeaderValueType = { authorization: string };
+
+export class BasicAuth {
+  static toHeaderValue(
+    username: BasicAuthUsernameType,
+    password: BasicAuthPasswordType,
+  ): BasicAuthHeaderValueType {
+    const credentials = btoa(`${username}:${password}`);
+
+    return { authorization: `Basic ${credentials}` };
+  }
+
+  static toHeader(username: BasicAuthUsernameType, password: BasicAuthPasswordType): Headers {
+    return new Headers(BasicAuth.toHeaderValue(username, password));
+  }
+}
--- tests/basic-auth.test.ts
+++ tests/basic-auth.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "bun:test";
 
-import { BasicAuthPassword, BasicAuthUsername } from "../src/basic-auth";
+import { BasicAuth, BasicAuthPassword, BasicAuthUsername } from "../src/basic-auth";
 
 describe("Basic auth validators", () => {
   test("BasicAuthUsername - valid", () => {
@@ -27,5 +27,22 @@ describe("Basic auth validators", () => {
   test("BasicAuthPassword - too long", () => {
     const longPassword = "p".repeat(129);
     expect(() => BasicAuthPassword.parse(longPassword)).toThrow();
+  });
+});
+
+describe("BasicAuth", () => {
+  const username = "admin";
+  const password = "password";
+
+  // cSpell:ignore YWRtaW46cGFzc3dvcmQ
+  const result = { authorization: "Basic YWRtaW46cGFzc3dvcmQ=" };
+
+  test("toHeaderValue", () => {
+    expect(BasicAuth.toHeaderValue(username, password)).toEqual(result);
+  });
+
+  test("toHeader", () => {
+    expect(BasicAuth.toHeader(username, password)).toBeInstanceOf(Headers);
+    expect(BasicAuth.toHeader(username, password).toJSON()).toEqual(result);
   });
 });
