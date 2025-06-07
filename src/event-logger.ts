4ad4d7132898eb6d363906649a3840219c03cc88
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-07 10:11:38 +0200

    refactor: extract event handler

---
+++ src/event-logger.ts
@@ -0,0 +1,24 @@
+import { Logger } from "./logger";
+
+export class EventLogger {
+  constructor(private readonly logger: Logger) {}
+
+  private _handle(
+    type: string,
+    _debugName: string,
+    eventName: string | undefined,
+    eventData: Record<string, any> | undefined,
+  ) {
+    if (type === "subscribe") return;
+
+    if (typeof eventName === "symbol") return;
+
+    this.logger.info({
+      message: `${eventName} emitted`,
+      operation: "event_emitted",
+      metadata: eventData,
+    });
+  }
+
+  handle = this._handle.bind(this);
+}
--- src/event.ts
+++ src/event.ts
@@ -1,6 +1,5 @@
 import { z } from "zod/v4";
 
-import { Logger } from "./logger";
 import { UUID } from "./uuid";
 
 export const Event = z.object({
@@ -28,26 +27,3 @@ export const ParsedEvent = Event.merge(z.object({ payload: z.record(z.string(), 
 
 export type EventType = z.infer<typeof Event>;
 export type ParsedEventType = z.infer<typeof ParsedEvent>;
-
-export class EventLogger {
-  constructor(private readonly logger: Logger) {}
-
-  private _handle(
-    type: string,
-    _debugName: string,
-    eventName: string | undefined,
-    eventData: Record<string, any> | undefined,
-  ) {
-    if (type === "subscribe") return;
-
-    if (typeof eventName === "symbol") return;
-
-    this.logger.info({
-      message: `${eventName} emitted`,
-      operation: "event_emitted",
-      metadata: eventData,
-    });
-  }
-
-  handle = this._handle.bind(this);
-}
