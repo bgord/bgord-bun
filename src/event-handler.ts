4b2f76d45557e52624f8aeaa4245c73d8ba23f5d
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-07 10:10:48 +0200

    refactor: extract event handler

---
+++ src/event-handler.ts
@@ -0,0 +1,20 @@
+import { EventType } from "./event";
+import { Logger } from "./logger";
+
+export class EventHandler {
+  constructor(private readonly logger: Logger) {}
+
+  handle<T extends Pick<EventType, "name">>(fn: (event: T) => Promise<void>) {
+    return async (event: T) => {
+      try {
+        await fn(event);
+      } catch (error) {
+        this.logger.error({
+          message: `Unknown ${event.name} event handler error`,
+          operation: "unknown_event_handler_error",
+          metadata: this.logger.formatError(error),
+        });
+      }
+    };
+  }
+}
--- src/event.ts
+++ src/event.ts
@@ -29,24 +29,6 @@ export const ParsedEvent = Event.merge(z.object({ payload: z.record(z.string(), 
 export type EventType = z.infer<typeof Event>;
 export type ParsedEventType = z.infer<typeof ParsedEvent>;
 
-export class EventHandler {
-  constructor(private readonly logger: Logger) {}
-
-  handle<T extends Pick<EventType, "name">>(fn: (event: T) => Promise<void>) {
-    return async (event: T) => {
-      try {
-        await fn(event);
-      } catch (error) {
-        this.logger.error({
-          message: `Unknown ${event.name} event handler error`,
-          operation: "unknown_event_handler_error",
-          metadata: this.logger.formatError(error),
-        });
-      }
-    };
-  }
-}
-
 export class EventLogger {
   constructor(private readonly logger: Logger) {}
 
