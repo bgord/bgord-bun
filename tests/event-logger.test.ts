ed63816dbe54f80baa50a34a849b18777e630af6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-07 10:45:00 +0200

    step: test event logger

---
+++ tests/event-logger.test.ts
@@ -0,0 +1,48 @@
+import { expect, spyOn, test } from "bun:test";
+
+import { EventLogger } from "../src/event-logger";
+
+class FakeLogger {
+  info = (_: any) => {};
+}
+
+test("logs emitted event with metadata", () => {
+  const logger = new FakeLogger();
+  const spy = spyOn(logger, "info");
+
+  const eventLogger = new EventLogger(logger as any);
+
+  eventLogger.handle("emit", "debug:name", "user.created", { userId: 123 });
+
+  expect(spy).toHaveBeenCalledWith({
+    message: "user.created emitted",
+    operation: "event_emitted",
+    metadata: { userId: 123 },
+  });
+});
+
+test("does not log subscribe events", () => {
+  const logger = new FakeLogger();
+  const spy = spyOn(logger, "info");
+
+  const eventLogger = new EventLogger(logger as any);
+
+  eventLogger.handle("subscribe", "debug:name", "user.created", {
+    userId: 123,
+  });
+
+  expect(spy).not.toHaveBeenCalled();
+});
+
+test("does not log events with symbol names", () => {
+  const logger = new FakeLogger();
+  const spy = spyOn(logger, "info");
+
+  const eventLogger = new EventLogger(logger as any);
+
+  eventLogger.handle("emit", "debug:name", Symbol("user.created") as any, {
+    userId: 123,
+  });
+
+  expect(spy).not.toHaveBeenCalled();
+});
