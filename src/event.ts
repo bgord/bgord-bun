11186b01a78dd9885c19d304fbe2da569867e90d
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-07 10:08:46 +0200

    step: migrate event

---
+++ src/event.ts
@@ -0,0 +1,71 @@
+import { z } from "zod/v4";
+
+import { Logger } from "./logger";
+import { UUID } from "./uuid";
+
+export const Event = z.object({
+  id: UUID,
+  createdAt: z.date(),
+
+  stream: z.string().min(1),
+
+  name: z.string().min(1),
+  version: z.number().int().positive(),
+  payload: z
+    .record(z.string(), z.any())
+    .refine((value) => {
+      try {
+        JSON.parse(String(value));
+        return true;
+      } catch (error) {
+        return false;
+      }
+    })
+    .transform((value) => JSON.stringify(value)),
+});
+
+export const ParsedEvent = Event.merge(z.object({ payload: z.record(z.string(), z.any()) }));
+
+export type EventType = z.infer<typeof Event>;
+export type ParsedEventType = z.infer<typeof ParsedEvent>;
+
+export class EventHandler {
+  constructor(private readonly logger: Logger) {}
+
+  handle<T extends Pick<EventType, "name">>(fn: (event: T) => Promise<void>) {
+    return async (event: T) => {
+      try {
+        await fn(event);
+      } catch (error) {
+        this.logger.error({
+          message: `Unknown ${event.name} event handler error`,
+          operation: "unknown_event_handler_error",
+          metadata: this.logger.formatError(error),
+        });
+      }
+    };
+  }
+}
+
+export class EventLogger {
+  constructor(private readonly logger: Logger) {}
+
+  private _handle(
+    type: string,
+    _debugName: string,
+    eventName: string | undefined,
+    eventData: Record<string, any> | undefined,
+  ) {
+    if (type === "subscribe") return;
+
+    if (typeof eventName === "symbol") return;
+
+    this.logger.info({
+      message: `${eventName} emitted`,
+      operation: "event_emitted",
+      metadata: eventData,
+    });
+  }
+
+  handle = this._handle.bind(this);
+}
