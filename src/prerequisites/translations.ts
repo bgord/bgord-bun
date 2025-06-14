f109ce97d48005ef3d6f671f640bbbfce2b1c12a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-14 12:42:22 +0200

    refactor: adjust translations prereq

--- src/prerequisites/translations.ts
+++ src/prerequisites/translations.ts
@@ -34,7 +34,10 @@ export class PrerequisiteTranslations extends prereqs.AbstractPrerequisite<Prere
       await fsp.access(translationsPath, constants.R_OK);
 
       for (const language in this.config.supportedLanguages) {
-        await fsp.access(new i18n.I18n().getTranslationPathForLanguage(language), constants.R_OK);
+        await fsp.access(
+          new i18n.I18n().getTranslationPathForLanguage(language as tools.LanguageType),
+          constants.R_OK,
+        );
       }
     } catch (error) {
       return this.reject();
@@ -49,28 +52,28 @@ export class PrerequisiteTranslations extends prereqs.AbstractPrerequisite<Prere
     const problems: PrerequisiteTranslationsProblemType[] = [];
 
     for (const language of supportedLanguages) {
-      const translations = await new i18n.I18n().getTranslations(language);
+      const translations = await new i18n.I18n().getTranslations(language as tools.LanguageType);
       const translationKeys = Object.keys(translations);
 
-      languageToTranslationKeys[language] = translationKeys;
+      languageToTranslationKeys[language as tools.LanguageType] = translationKeys;
     }
 
     for (const currentLanguage in languageToTranslationKeys) {
-      const translationKeys = languageToTranslationKeys[currentLanguage] ?? [];
+      const translationKeys = languageToTranslationKeys[currentLanguage as tools.LanguageType] ?? [];
 
       for (const translationKey of translationKeys) {
         for (const supportedLanguage of supportedLanguages) {
           if (supportedLanguage === currentLanguage) continue;
 
-          const translationKeyExists = languageToTranslationKeys[supportedLanguage]?.some(
-            (key) => translationKey === key,
-          );
+          const translationKeyExists = languageToTranslationKeys[
+            supportedLanguage as tools.LanguageType
+          ]?.some((key) => translationKey === key);
 
           if (!translationKeyExists) {
             problems.push({
               translationKey,
-              existsInLanguage: currentLanguage,
-              missingInLanguage: supportedLanguage,
+              existsInLanguage: currentLanguage as tools.LanguageType,
+              missingInLanguage: supportedLanguage as tools.LanguageType,
             });
           }
         }
