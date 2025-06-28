ad38e06737a617f533a79cab347306a99459d032
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-28 18:07:20 +0200

    step: add correlationId to commands

--- src/command.types.ts
+++ src/command.types.ts
@@ -2,6 +2,7 @@ import { z } from "zod/v4";
 
 export type GenericCommandSchema = z.ZodObject<{
   id: z.ZodType<string>;
+  correlationId: z.ZodType<string>;
   createdAt: z.ZodType<number>;
   name: z.ZodLiteral<string>;
   payload: z.ZodType<any>;
