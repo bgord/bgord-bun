1f537b9349eb0cd172a900c9934f374782b29c49
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-01 09:57:06 +0200

    step: add node cache rls adapter

---
+++ src/node-cache-rate-limit-store.adapter.ts
@@ -0,0 +1,24 @@
+import * as tools from "@bgord/tools";
+import NodeCache from "node-cache";
+
+import { RateLimitStore, RateLimitStoreSubjectType } from "./rate-limit-store.port";
+
+export class NodeCacheRateLimitStore implements RateLimitStore {
+  private readonly store: NodeCache;
+
+  constructor(private readonly time: tools.TimeResult) {
+    this.store = new NodeCache({
+      stdTTL: this.time.seconds,
+      checkperiod: this.time.seconds,
+      deleteOnExpire: true,
+      maxKeys: 100_000,
+    });
+  }
+
+  get(subject: RateLimitStoreSubjectType) {
+    return this.store.get<tools.RateLimiter>(subject);
+  }
+  set(subject: RateLimitStoreSubjectType, limiter: tools.RateLimiter) {
+    return this.store.set(subject, limiter);
+  }
+}
