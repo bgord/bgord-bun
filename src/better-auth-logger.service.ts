8d3348dc066439c1392658b58479c5bd7f8bbbbb
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-14 21:07:27 +0200

    refactor: improve the better auth logger

--- src/better-auth-logger.service.ts
+++ src/better-auth-logger.service.ts
@@ -1,15 +1,5 @@
+import type { LogLevel } from "better-auth";
 import { Logger, LogLevelEnum } from "./logger.service";
-
-const mapLevel = (level: string) =>
-  level === "error"
-    ? LogLevelEnum.error
-    : level === "warn"
-      ? LogLevelEnum.warn
-      : level === "http"
-        ? LogLevelEnum.info
-        : level === "debug"
-          ? LogLevelEnum.info
-          : LogLevelEnum.info;
 
 export class BetterAuthLogger {
   constructor(private readonly logger: Logger) {}
@@ -18,12 +8,21 @@ export class BetterAuthLogger {
     return {
       disabled: false,
       level: "debug",
-      log: (level: string, message: string, ...args: unknown[]) =>
-        this.logger[mapLevel(level)]({
+      log: (level: LogLevel | undefined, message: string, ...args: unknown[]) =>
+        this.logger[this.mapLevel(level)]({
           message,
           operation: "better-auth",
           metadata: { args },
         }),
-    };
+    } as const;
+  }
+
+  private mapLevel(level: LogLevel | undefined) {
+    if (level === "info") return LogLevelEnum.info;
+    if (level === "success") return LogLevelEnum.info;
+    if (level === "warn") return LogLevelEnum.warn;
+    if (level === "error") return LogLevelEnum.error;
+    if (level === "debug") return LogLevelEnum.info;
+    return LogLevelEnum.info;
   }
 }
