bcc3955da679be156b0c631474b536a4ac8cc0c8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-24 01:21:14 +0200

    refactor: adjust http logger

--- tests/http-logger.test.ts
+++ tests/http-logger.test.ts
@@ -3,8 +3,7 @@ import { Hono } from "hono";
 import { requestId } from "hono/request-id";
 import { timing } from "hono/timing";
 import { HttpLogger } from "../src/http-logger.middleware";
-import { Logger } from "../src/logger.service";
-import { NodeEnvironmentEnum } from "../src/node-env.vo";
+import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
 
 const ip = {
   server: {
@@ -14,10 +13,7 @@ const ip = {
 
 describe("HttpLogger middleware", () => {
   test("logs correct 200 HTTP log", async () => {
-    const logger = new Logger({
-      app: "bgord-bun-api",
-      environment: NodeEnvironmentEnum.local,
-    });
+    const logger = new LoggerNoopAdapter();
 
     const loggerHttpSpy = spyOn(logger, "http").mockImplementation(jest.fn());
 
@@ -35,6 +31,7 @@ describe("HttpLogger middleware", () => {
     expect(loggerHttpSpy).toHaveBeenNthCalledWith(
       1,
       expect.objectContaining({
+        component: "http",
         operation: "http_request_before",
         correlationId: expect.stringMatching(
           /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/,
@@ -50,6 +47,7 @@ describe("HttpLogger middleware", () => {
     expect(loggerHttpSpy).toHaveBeenNthCalledWith(
       2,
       expect.objectContaining({
+        component: "http",
         operation: "http_request_after",
         correlationId: expect.stringMatching(
           /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/,
@@ -57,7 +55,7 @@ describe("HttpLogger middleware", () => {
         message: "response",
         method: "GET",
         url: "http://localhost/ping",
-        responseCode: 200,
+        status: 200,
         durationMs: expect.any(Number),
         client: { ip: "127.0.0.1", userAgent: undefined },
         metadata: { response: { message: "OK" } },
@@ -68,10 +66,7 @@ describe("HttpLogger middleware", () => {
   });
 
   test("logs correct 400 HTTP log", async () => {
-    const logger = new Logger({
-      app: "bgord-bun-api",
-      environment: NodeEnvironmentEnum.local,
-    });
+    const logger = new LoggerNoopAdapter();
 
     const loggerHttpSpy = spyOn(logger, "http").mockImplementation(jest.fn());
 
@@ -113,7 +108,7 @@ describe("HttpLogger middleware", () => {
         message: "response",
         method: "GET",
         url: "http://localhost/ping",
-        responseCode: 500,
+        status: 500,
         durationMs: expect.any(Number),
         client: { ip: "127.0.0.1", userAgent: undefined },
         metadata: { response: { message: "general.unknown" } },
