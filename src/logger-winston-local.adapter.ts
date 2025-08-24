4d395b16879343c88777e47559fa877969a367fe
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-24 10:56:02 +0200

    refactor: adjust local adapter config

--- src/logger-winston-local.adapter.ts
+++ src/logger-winston-local.adapter.ts
@@ -3,12 +3,14 @@ import type { LogAppType, LoggerPort, LogLevelEnum } from "./logger.port";
 import { LoggerWinstonAdapter } from "./logger-winston.adapter";
 import { NodeEnvironmentEnum } from "./node-env.vo";
 
+type LoggerWinstonLocalAdapterConfigType = { app: LogAppType };
+
 export class LoggerWinstonLocalAdapter {
-  constructor(private readonly app: LogAppType) {}
+  constructor(private readonly config: LoggerWinstonLocalAdapterConfigType) {}
 
   create(level: LogLevelEnum): LoggerPort {
     return new LoggerWinstonAdapter({
-      app: this.app,
+      app: this.config.app,
       environment: NodeEnvironmentEnum.local,
       level,
       formats: [winston.format.prettyPrint()],
