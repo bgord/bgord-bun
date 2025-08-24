2dc0ecba5aa1dbefedb22126bed11d1036990bfd
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-24 12:24:28 +0200

    step: define client vo

--- src/client.vo.ts
+++ src/client.vo.ts
@@ -1,6 +1,16 @@
+export type ClientIpType = string;
+export type ClientUaType = string;
+
+export type ClientType = { ip: ClientIpType; ua: ClientUaType };
+
 export class Client {
-  constructor(
-    readonly ip: string,
-    readonly ua: string,
-  ) {}
+  private constructor(private readonly value: ClientType) {}
+
+  static from(ip: ClientIpType | null | undefined, ua: ClientUaType | null | undefined): Client {
+    return new Client({ ip: ip ?? "anon", ua: ua ?? "anon" });
+  }
+
+  toJSON(): ClientType {
+    return this.value;
+  }
 }
