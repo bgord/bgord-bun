42a7848587f15452935d52d828326214fb3821a5
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-26 22:46:35 +0200

    refactor: file hash return type

--- src/file-hash-noop.adapter.ts
+++ src/file-hash-noop.adapter.ts
@@ -1,8 +1,8 @@
 import * as tools from "@bgord/tools";
-import type { FileHashPort, FileHashResult } from "./file-hash.port";
+import type { FileHashPort } from "./file-hash.port";
 
 export class FileHashNoopAdapter implements FileHashPort {
-  async hash(_path: tools.FilePathAbsolute | tools.FilePathRelative): Promise<FileHashResult> {
-    return { hex: "noop", size: tools.Size.fromBytes(0), lastModified: tools.Timestamp.parse(1000) };
+  async hash(_path: tools.FilePathAbsolute | tools.FilePathRelative) {
+    return { etag: "noop", size: tools.Size.fromBytes(0), lastModified: tools.Timestamp.parse(1000) };
   }
 }
--- src/file-hash-sha256-bun.adapter.ts
+++ src/file-hash-sha256-bun.adapter.ts
@@ -1,16 +1,16 @@
 import * as tools from "@bgord/tools";
-import type { FileHashPort, FileHashResult } from "./file-hash.port";
+import type { FileHashPort } from "./file-hash.port";
 
 export class FileHashSha256BunAdapter implements FileHashPort {
-  async hash(path: tools.FilePathAbsolute | tools.FilePathRelative): Promise<FileHashResult> {
+  async hash(path: tools.FilePathAbsolute | tools.FilePathRelative) {
     const file = Bun.file(path.get());
 
     const arrayBuffer = await file.arrayBuffer();
     const digest = await crypto.subtle.digest("SHA-256", arrayBuffer);
-    const hex = Buffer.from(digest).toString("hex");
+    const etag = Buffer.from(digest).toString("hex");
 
     return {
-      hex,
+      etag,
       size: tools.Size.fromBytes(arrayBuffer.byteLength),
       lastModified: tools.Timestamp.parse(file.lastModified),
     };
