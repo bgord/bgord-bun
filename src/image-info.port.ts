8694b30223e3d1fe2616e2cd8ba9c5f27641f509
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-26 20:16:15 +0200

    refactor: export image info type

--- src/image-info.port.ts
+++ src/image-info.port.ts
@@ -1,6 +1,11 @@
 import type * as tools from "@bgord/tools";
 
-type ImageInfoType = { height: tools.HeightType; width: tools.WidthType; mime: tools.Mime; size: tools.Size };
+export type ImageInfoType = {
+  height: tools.HeightType;
+  width: tools.WidthType;
+  mime: tools.Mime;
+  size: tools.Size;
+};
 
 export interface ImageInfoPort {
   inspect(filePath: tools.FilePathRelative | tools.FilePathAbsolute): Promise<ImageInfoType>;
