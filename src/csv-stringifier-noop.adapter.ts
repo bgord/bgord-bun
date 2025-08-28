e1012e8cb536ead712b81ffc3dbaf6edc6c58983
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-28 22:39:28 +0200

    step: add exports

--- src/csv-stringifier-noop.adapter.ts
+++ src/csv-stringifier-noop.adapter.ts
@@ -1,6 +1,6 @@
 import type { CsvColumnType, CsvRowType, CsvStringifierPort } from "./csv-stringifier.port";
 
-export class CsvStringifierSyncAdapter implements CsvStringifierPort {
+export class CsvStringifierNoopAdapter implements CsvStringifierPort {
   process(_columns: CsvColumnType[], _data: CsvRowType[]) {
     return "noop";
   }
