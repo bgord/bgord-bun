0dfaa2ad703fd13fecbbc42ede7ea552880419cf
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-28 22:36:06 +0200

    step: add csv stringifier port

---
+++ src/csv-stringifier.port.ts
@@ -0,0 +1,7 @@
+export type CsvColumnType = string;
+
+export type CsvRowType = Record<string, any>;
+
+export interface CsvStringifierPort {
+  process(columns: CsvColumnType[], data: CsvRowType[]): string;
+}
