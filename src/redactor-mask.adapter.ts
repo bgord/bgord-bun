3323dd27db9ff27bf618b36b64cb61e1734fca5f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-22 22:41:41 +0200

    step: define redactor mask default keys

--- src/redactor-mask.adapter.ts
+++ src/redactor-mask.adapter.ts
@@ -2,7 +2,29 @@ import cloneDeepWith from "lodash/cloneDeepWith";
 import type { RedactorPort } from "./redactor.port";
 
 export class RedactorMaskAdapter implements RedactorPort {
-  // TODO static INITIAL_KEYS
+  static readonly DEFAULT_KEYS: readonly string[] = [
+    // auth & cookies
+    "authorization",
+    "cookie",
+    "set-cookie",
+    "x-api-key",
+    "apiKey",
+    "token",
+    "accessToken",
+    "refreshToken",
+
+    // passwords & secrets
+    "password",
+    "currentPassword",
+    "newPassword",
+    "passwordConfirmation",
+    "clientSecret",
+    "secret",
+
+    // one-time codes
+    "otp",
+    "code",
+  ];
 
   private readonly keys: Set<string>;
 
