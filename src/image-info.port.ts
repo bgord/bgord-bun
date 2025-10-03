b974ae40428ea7c59aa4c81ddc753c1fefe4432c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-03 11:43:15 +0200

    fix: image-info port types

--- src/image-info.port.ts
+++ src/image-info.port.ts
@@ -1,8 +1,8 @@
 import type * as tools from "@bgord/tools";
 
 export type ImageInfoType = {
-  height: tools.HeightType;
-  width: tools.WidthType;
+  height: tools.ImageHeightType;
+  width: tools.ImageWidthType;
   mime: tools.Mime;
   size: tools.Size;
 };
