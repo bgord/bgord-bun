1b3a01e6b40299b88090ea0e928dd951c3e8ae82
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-07 22:39:03 +0200

    refactor: make noop inspector dynamic

--- src/certificate-inspector-noop.adapter.ts
+++ src/certificate-inspector-noop.adapter.ts
@@ -1,7 +1,9 @@
 import type { CertificateInspection, CertificateInspectorPort } from "./certificate-inspector.port";
 
 export class CertificateInspectorNoopAdapter implements CertificateInspectorPort {
+  constructor(private readonly daysRemaining: number) {}
+
   async inspect(_hostname: string): Promise<CertificateInspection> {
-    return { success: true, daysRemaining: 7 };
+    return { success: true, daysRemaining: this.daysRemaining };
   }
 }
