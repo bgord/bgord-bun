7c7e92e66d8cdde18835d7ae1dc4c39b69731460
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-15 12:46:22 +0200

    refactor: improve event logger

---
+++ tests/event-logger.service.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, spyOn, test } from "bun:test";
+import { EventLogger } from "../src/event-logger.service";
+import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+
+const logger = new LoggerNoopAdapter();
+const commandLogger = new EventLogger(logger);
+const eventName = "user.created";
+
+describe("EventLogger service", () => {
+  test("happy path", () => {
+    const loggerInfoSpy = spyOn(logger, "info");
+    const eventLogger = new EventLogger(logger);
+
+    eventLogger.handle("emit", "debug:name", eventName, { userId: 123 });
+
+    expect(loggerInfoSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: `${eventName} emitted`,
+        component: "infra",
+        operation: "event_emitted",
+        metadata: { userId: 123 },
+      }),
+    );
+  });
+
+  test("does not log subscribe commands", () => {
+    const loggerInfo = spyOn(logger, "info");
+
+    commandLogger.handle("subscribe", "debug:name", eventName, { userId: 123 });
+
+    expect(loggerInfo).not.toHaveBeenCalled();
+  });
+
+  test("does not log commands with symbol names", () => {
+    const loggerInfo = spyOn(logger, "info");
+
+    commandLogger.handle("emit", "debug:name", Symbol(eventName), { userId: 123 });
+
+    expect(loggerInfo).not.toHaveBeenCalled();
+  });
+});
