221c34119329b323d344336fa2dd1c1c75ef9f53
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-15 11:04:14 +0200

    refactor: unify node tests

--- tests/prerequisites-node.test.ts
+++ tests/prerequisites-node.test.ts
@@ -3,38 +3,42 @@ import * as tools from "@bgord/tools";
 import { PrerequisiteNode } from "../src/prerequisites/node";
 import * as prereqs from "../src/prerequisites.service";
 
-const base = tools.PackageVersion.fromString("20.0.0");
+const version = tools.PackageVersion.fromString("20.0.0");
 
 describe("prerequisites - node", () => {
-  test("passes if current Node.js version is sufficient", async () => {
-    const prerequisite = new PrerequisiteNode({ label: "node", version: base, current: "v20.10.0" });
-
-    expect(await prerequisite.verify()).toEqual(prereqs.Verification.success());
+  test("success - Node.js version is equal", async () => {
+    expect(await new PrerequisiteNode({ label: "node", version, current: "v20.0.0" }).verify()).toEqual(
+      prereqs.Verification.success(),
+    );
   });
 
-  test("fails if current Node.js version is too low", async () => {
-    const prerequisite = new PrerequisiteNode({ label: "node", version: base, current: "v18.10.0" });
-
-    // @ts-expect-error
-    expect((await prerequisite.verify()).error.message).toEqual("Version: v18.10.0");
+  test("success - Node.js version is greater", async () => {
+    expect(await new PrerequisiteNode({ label: "node", version, current: "v20.10.0" }).verify()).toEqual(
+      prereqs.Verification.success(),
+    );
   });
 
-  test("fails if invalid Node.js version is passed", async () => {
-    const prerequisite = new PrerequisiteNode({ label: "node", version: base, current: "abc" });
+  test("failure - Node.js version is too low", async () => {
+    expect(
+      // @ts-expect-error
+      (await new PrerequisiteNode({ label: "node", version, current: "v18.10.0" }).verify()).error.message,
+    ).toEqual("Version: v18.10.0");
+  });
 
-    expect(await prerequisite.verify()).toEqual(
+  test("failure - invalid Node.js version is passed", async () => {
+    expect(await new PrerequisiteNode({ label: "node", version, current: "abc" }).verify()).toEqual(
       prereqs.Verification.failure({ message: "Invalid version passed: abc" }),
     );
   });
 
   test("undetermined", async () => {
-    const prerequisite = new PrerequisiteNode({
-      label: "node",
-      version: base,
-      current: "v20.0.0",
-      enabled: false,
-    });
-
-    expect(await prerequisite.verify()).toEqual(prereqs.Verification.undetermined());
+    expect(
+      await new PrerequisiteNode({
+        label: "node",
+        version,
+        current: "v20.0.0",
+        enabled: false,
+      }).verify(),
+    ).toEqual(prereqs.Verification.undetermined());
   });
 });
