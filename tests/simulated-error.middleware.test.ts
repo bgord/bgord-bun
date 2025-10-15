746ef605548dda1762c782e1e07586dc09071e3c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-15 12:01:07 +0200

    refactor: improve simulated error

--- tests/simulated-error.middleware.test.ts
+++ tests/simulated-error.middleware.test.ts
@@ -2,10 +2,9 @@ import { describe, expect, test } from "bun:test";
 import { Hono } from "hono";
 import { SimulatedError } from "../src/simulated-error.middleware";
 
-describe("SimulatedError", () => {
-  test("works", async () => {
-    const app = new Hono();
-    app.get("/simulated-error", SimulatedError.handle, (_c) => expect.unreachable());
+describe("SimulatedError middleware", () => {
+  test("happy path", async () => {
+    const app = new Hono().get("/simulated-error", SimulatedError.handle, (_c) => expect.unreachable());
     await app.request("/simulated-error");
   });
 });
