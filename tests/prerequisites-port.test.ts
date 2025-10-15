6503b63199dea4ffda58f1fa0573a19de56abbfa
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-15 11:06:20 +0200

    refactor: unify port tests

--- tests/prerequisites-port.test.ts
+++ tests/prerequisites-port.test.ts
@@ -3,27 +3,28 @@ import { Port } from "../src/port.vo";
 import { PrerequisitePort } from "../src/prerequisites/port";
 import * as prereqs from "../src/prerequisites.service";
 
-const PORT = Port.parse(43210);
+const port = Port.parse(43210);
 
 describe("prerequisites - port", () => {
-  test("passes if port is available", async () => {
-    const prerequisite = new PrerequisitePort({ port: PORT, label: "port" });
-
-    expect(await prerequisite.verify()).toEqual(prereqs.Verification.success());
+  test("success", async () => {
+    expect(await new PrerequisitePort({ port, label: "port" }).verify()).toEqual(
+      prereqs.Verification.success(),
+    );
   });
 
-  test("fails if port is in use", async () => {
-    const occupied = Bun.listen({ hostname: "::", port: PORT, socket: { data() {} } });
+  test("failure", async () => {
+    const occupied = Bun.listen({ hostname: "::", port, socket: { data() {} } });
 
-    const prerequisite = new PrerequisitePort({ port: PORT, label: "port" });
+    expect(await new PrerequisitePort({ port, label: "port" }).verify()).toEqual(
+      prereqs.Verification.failure(),
+    );
 
-    expect(await prerequisite.verify()).toEqual(prereqs.Verification.failure());
     occupied.stop();
   });
 
   test("undetermined", async () => {
-    const prerequisite = new PrerequisitePort({ port: PORT, label: "port", enabled: false });
-
-    expect(await prerequisite.verify()).toEqual(prereqs.Verification.undetermined());
+    expect(await new PrerequisitePort({ port, label: "port", enabled: false }).verify()).toEqual(
+      prereqs.Verification.undetermined(),
+    );
   });
 });
