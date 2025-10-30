7e168262530e062842f2850740e00b6378fec586
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-30 13:17:23 +0100

    fix: history reader port read return type

--- src/modules/history/ports/history-reader.ts
+++ src/modules/history/ports/history-reader.ts
@@ -1,5 +1,5 @@
 import type * as VO from "../value-objects";
 
 export interface HistoryReaderPort {
-  read(subject: VO.HistoryParsedType["subject"]): Promise<VO.HistoryType>;
+  read(subject: VO.HistoryParsedType["subject"]): Promise<VO.HistoryType[]>;
 }
