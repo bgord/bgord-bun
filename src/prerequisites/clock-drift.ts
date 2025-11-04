b9df8a5fd577668ac4b0fd758aa487497f4b9f45
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-04 14:19:10 +0100

    step: define skew

--- src/prerequisites/clock-drift.ts
+++ src/prerequisites/clock-drift.ts
@@ -1,3 +1,4 @@
+import type * as tools from "@bgord/tools";
 import * as prereqs from "../prerequisites.service";
 
 export class PrerequisiteClockDrift implements prereqs.Prerequisite {
@@ -5,9 +6,13 @@ export class PrerequisiteClockDrift implements prereqs.Prerequisite {
   readonly label: prereqs.PrerequisiteLabelType;
   readonly enabled?: boolean = true;
 
-  constructor(config: prereqs.PrerequisiteConfigType) {
+  readonly skew: tools.Duration;
+
+  constructor(config: prereqs.PrerequisiteConfigType & { skew: tools.Duration }) {
     this.label = config.label;
     this.enabled = config.enabled === undefined ? true : config.enabled;
+
+    this.skew = config.skew;
   }
 
   async verify(): Promise<prereqs.VerifyOutcome> {
