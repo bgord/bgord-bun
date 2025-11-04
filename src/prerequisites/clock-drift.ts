6e6628d2736f7acb47b5747b41afdabcbd4b7da6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-04 14:27:14 +0100

    step: accept clock port in clock drift

--- src/prerequisites/clock-drift.ts
+++ src/prerequisites/clock-drift.ts
@@ -1,4 +1,5 @@
 import type * as tools from "@bgord/tools";
+import type { ClockPort } from "../clock.port";
 import * as prereqs from "../prerequisites.service";
 import type { TimekeeperPort } from "../timekeeper.port";
 
@@ -9,13 +10,21 @@ export class PrerequisiteClockDrift implements prereqs.Prerequisite {
 
   readonly skew: tools.Duration;
   readonly timekeeper: TimekeeperPort;
+  readonly clock: ClockPort;
 
-  constructor(config: prereqs.PrerequisiteConfigType & { skew: tools.Duration; timekeeper: TimekeeperPort }) {
+  constructor(
+    config: prereqs.PrerequisiteConfigType & {
+      skew: tools.Duration;
+      timekeeper: TimekeeperPort;
+      clock: ClockPort;
+    },
+  ) {
     this.label = config.label;
     this.enabled = config.enabled === undefined ? true : config.enabled;
 
     this.skew = config.skew;
     this.timekeeper = config.timekeeper;
+    this.clock = config.clock;
   }
 
   async verify(): Promise<prereqs.VerifyOutcome> {
