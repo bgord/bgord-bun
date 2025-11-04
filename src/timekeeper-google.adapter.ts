caa8e21e0198ddbea093f99f649fa6e8e17194ce
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-04 15:17:01 +0100

    step: implement timekeeper google adapter

---
+++ src/timekeeper-google.adapter.ts
@@ -0,0 +1,14 @@
+import * as tools from "@bgord/tools";
+import type { TimekeeperPort } from "./timekeeper.port";
+
+export class TimekeeperGoogleAdapter implements TimekeeperPort {
+  async get() {
+    const response = await fetch("https://www.google.com/generate_204");
+    if (!response.ok) return null;
+
+    const date = response.headers.get("Date");
+    if (!date) return null;
+
+    return tools.Timestamp.fromNumber(new Date(date).getTime());
+  }
+}
