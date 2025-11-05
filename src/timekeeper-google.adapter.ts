839204a38ad1cd5b2dda957f13ebbc779dae8420
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-05 12:19:40 +0100

    step: extract timekeeper google adapter url

--- src/timekeeper-google.adapter.ts
+++ src/timekeeper-google.adapter.ts
@@ -2,9 +2,11 @@ import * as tools from "@bgord/tools";
 import type { TimekeeperPort } from "./timekeeper.port";
 
 export class TimekeeperGoogleAdapter implements TimekeeperPort {
+  static URL = "https://www.google.com/generate_204";
+
   async get() {
     try {
-      const response = await fetch("https://www.google.com/generate_204");
+      const response = await fetch(TimekeeperGoogleAdapter.URL);
       if (!response.ok) return null;
 
       const date = response.headers.get("Date");
