b05fdb712c6ce1fa5bc6b8681f4da11e25025775
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-06 11:03:48 +0100

    step: add gzip bun adapter

---
+++ src/gzip-bun.adapter.ts
@@ -0,0 +1,10 @@
+import type { GzipPort, GzipRecipe } from "./gzip.port";
+
+export class GzipBunAdapter implements GzipPort {
+  async pack(recipe: GzipRecipe) {
+    const file = await Bun.file(recipe.input.get()).arrayBuffer();
+    const archive = Bun.gzipSync(file);
+    await Bun.write(recipe.output.get(), archive);
+    return recipe.output;
+  }
+}
