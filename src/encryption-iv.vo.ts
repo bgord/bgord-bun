1ff92ec0aaef510c512c5c0a46fc9ebc0dbb64df
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-06 14:35:18 +0100

    refactor: rewrite encryption iv

--- src/encryption-iv.vo.ts
+++ src/encryption-iv.vo.ts
@@ -1,18 +1,8 @@
-import { EncryptionIvValue, type EncryptionIvValueType } from "./encryption-iv-value.vo";
-
 export class EncryptionIV {
-  static generate(): EncryptionIvValueType {
+  static generate(): Uint8Array {
     const iv = new Uint8Array(12);
     crypto.getRandomValues(iv);
 
-    let hex = "";
-
-    for (const byte of iv) {
-      const part = byte.toString(16);
-
-      hex += part.length === 1 ? "0" + part : part;
-    }
-
-    return EncryptionIvValue.parse(hex);
+    return iv;
   }
 }
--- tests/encryption-iv.vo.test.ts
+++ tests/encryption-iv.vo.test.ts
@@ -1,9 +1,8 @@
 import { describe, expect, test } from "bun:test";
 import { EncryptionIV } from "../src/encryption-iv.vo";
-import { EncryptionIvValue } from "../src/encryption-iv-value.vo";
 
 describe("EncryptionIV", () => {
   test("generate", () => {
-    expect(EncryptionIvValue.safeParse(EncryptionIV.generate()).success).toEqual(true);
+    expect(EncryptionIV.generate().length).toEqual(12);
   });
 });
