da239783520084644a4e10e555da4e96251d3c81
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-06 13:58:43 +0100

    step: add crypto key provider env adapter

--- src/crypto-key-provider-env.adapter.ts
+++ src/crypto-key-provider-env.adapter.ts
@@ -1,5 +1,16 @@
 import type { CryptoKeyProviderPort } from "./crypto-key-provider.port";
+import type { EncryptionKeyType } from "./encryption-key.vo";
 
-export class CryptoKeyProviderEnv implements CryptoKeyProviderPort {
-  async get(): Promise<CryptoKey> {}
+export class CryptoKeyProviderEnvAdapter implements CryptoKeyProviderPort {
+  constructor(private readonly ENCRYPTION_KEY: EncryptionKeyType) {}
+
+  async get(): Promise<CryptoKey> {
+    const bytes = new Uint8Array(32);
+
+    for (let i = 0; i < 32; i++) {
+      bytes[i] = Number.parseInt(this.ENCRYPTION_KEY.slice(i * 2, i * 2 + 2), 16);
+    }
+
+    return crypto.subtle.importKey("raw", bytes, { name: "AES-GCM" }, false, ["encrypt", "decrypt"]);
+  }
 }
