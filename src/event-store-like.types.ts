70d11a60a4485eae7021de1312f90803e022608c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-04 11:58:17 +0100

    refactor: unify event store like offset type

--- src/event-store-like.types.ts
+++ src/event-store-like.types.ts
@@ -1,5 +1,7 @@
+import type * as tools from "@bgord/tools";
+
 export type EventStoreLike<E extends { name: string }> = {
   save(events: E[]): Promise<unknown>;
 
-  saveAfter(events: E[], delay: { ms: number }): Promise<unknown>;
+  saveAfter(events: E[], offset: tools.Duration): Promise<unknown>;
 };
