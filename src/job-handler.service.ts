413085e01df61c8f8a7ea7140be79c0613a85be7
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-12 23:44:46 +0100

    refactor: remove unused this alias

--- src/job-handler.service.ts
+++ src/job-handler.service.ts
@@ -19,25 +19,22 @@ export class JobHandler {
   handle(jobProcessor: JobProcessorType) {
     const correlationId = this.deps.IdProvider.generate();
 
-    // biome-ignore lint: lint/complexity/noUselessThisAlias
-    const that = this;
-
     return async () => {
       const stopwatch = new tools.Stopwatch(this.deps.Clock.now());
 
       try {
-        that.deps.Logger.info({ message: `${jobProcessor.label} start`, correlationId, ...this.base });
+        this.deps.Logger.info({ message: `${jobProcessor.label} start`, correlationId, ...this.base });
 
         await CorrelationStorage.run(correlationId, jobProcessor.process);
 
-        that.deps.Logger.info({
+        this.deps.Logger.info({
           message: `${jobProcessor.label} success`,
           correlationId,
           metadata: stopwatch.stop(),
           ...this.base,
         });
       } catch (error) {
-        that.deps.Logger.error({
+        this.deps.Logger.error({
           message: `${jobProcessor.label} error`,
           correlationId,
           error: formatError(error),
@@ -49,9 +46,6 @@ export class JobHandler {
   }
 
   protect(cron: Cron) {
-    // biome-ignore lint: lint/complexity/noUselessThisAlias
-    const that = this;
-
-    return async () => that.deps.Logger.info({ message: `${cron.name} overrun`, ...this.base });
+    return async () => this.deps.Logger.info({ message: `${cron.name} overrun`, ...this.base });
   }
 }
