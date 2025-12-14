efb3c81e7ac3ad517b0f2f9d6134c6e226a81815
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-14 18:53:42 +0100

    step: change handle set user language command parameters

--- src/modules/preferences/command-handlers/handleSetUserLanguageCommand.ts
+++ src/modules/preferences/command-handlers/handleSetUserLanguageCommand.ts
@@ -9,27 +9,28 @@ import * as Invariants from "../invariants";
 import type * as Ports from "../ports";
 import type * as VO from "../value-objects";
 
+type Dependencies = {
+  EventStore: EventStoreLike<AcceptedEvent>;
+  IdProvider: IdProviderPort;
+  Clock: ClockPort;
+  UserLanguageQuery: Ports.UserLanguageQueryPort;
+};
+
 type AcceptedEvent = Events.UserLanguageSetEventType;
 
 export const handleSetUserLanguageCommand =
-  <L extends readonly tools.LanguageType[]>(
-    EventStore: EventStoreLike<AcceptedEvent>,
-    IdProvider: IdProviderPort,
-    Clock: ClockPort,
-    query: Ports.UserLanguageQueryPort,
-    supported: VO.SupportedLanguagesSet<L>,
-  ) =>
+  <L extends readonly tools.LanguageType[]>(supported: VO.SupportedLanguagesSet<L>, deps: Dependencies) =>
   async (command: Commands.SetUserLanguageCommandType) => {
     const candidate = supported.ensure(command.payload.language);
-    const current = await query.get(command.payload.userId);
+    const current = await deps.UserLanguageQuery.get(command.payload.userId);
 
     if (Invariants.UserLanguageHasChanged.fails({ current, candidate: command.payload.language })) return;
 
     const event = Events.UserLanguageSetEvent.parse({
-      ...createEventEnvelope(`preferences_${command.payload.userId}`, { IdProvider, Clock }),
+      ...createEventEnvelope(`preferences_${command.payload.userId}`, deps),
       name: Events.USER_LANGUAGE_SET_EVENT,
       payload: { userId: command.payload.userId, language: candidate },
     } satisfies Events.UserLanguageSetEventType);
 
-    await EventStore.save([event]);
+    await deps.EventStore.save([event]);
   };
