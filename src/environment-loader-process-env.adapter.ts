977615923793cbb9ebb8a5d8e15dd0e2a4eddc33
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-15 12:57:56 +0100

    refactor: simplify process env loader

--- src/environment-loader-process-env.adapter.ts
+++ src/environment-loader-process-env.adapter.ts
@@ -1,28 +1,22 @@
 import type { z } from "zod/v4";
-import { NodeEnvironment, type NodeEnvironmentEnum } from "../src/node-env.vo";
+import type { NodeEnvironmentEnum } from "../src/node-env.vo";
 import type { EnvironmentLoaderPort } from "./environment-loader.port";
 
 export class EnvironmentLoaderProcessEnvAdapter<Schema extends z.ZodObject<any>>
   implements EnvironmentLoaderPort<Schema>
 {
-  private readonly type: NodeEnvironmentEnum;
-  private readonly schema: Schema;
-
   constructor(
-    config: { type: typeof process.env.NODE_ENV; schema: Schema },
+    private readonly config: { type: NodeEnvironmentEnum; schema: Schema },
     private env: NodeJS.ProcessEnv,
-  ) {
-    this.schema = config.schema;
-    this.type = NodeEnvironment.parse(config.type);
-  }
+  ) {}
 
   async load() {
-    const result = this.schema.parse(this.env);
+    const result = this.config.schema.parse(this.env);
 
     for (const key of Object.keys(result)) {
       delete process.env[key];
     }
 
-    return Object.freeze({ ...result, type: this.type });
+    return Object.freeze({ ...result, type: this.config.type });
   }
 }
--- tests/environment-loader-process-env.adapter.test.ts
+++ tests/environment-loader-process-env.adapter.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from "bun:test";
 import { z } from "zod/v4";
 import { EnvironmentLoaderProcessEnvAdapter } from "../src/environment-loader-process-env.adapter";
-import { NodeEnvironmentEnum, NodeEnvironmentError } from "../src/node-env.vo";
+import { NodeEnvironmentEnum } from "../src/node-env.vo";
 
 const Schema = z.object({ APP_NAME: z.string() });
 
 describe("EnvironmentLoaderProcessEnv", () => {
   test("happy path", async () => {
     const result = await new EnvironmentLoaderProcessEnvAdapter(
-      { type: "local", schema: Schema },
+      { type: NodeEnvironmentEnum.local, schema: Schema },
       { ...process.env, APP_NAME: "MyApp" },
     ).load();
 
@@ -18,7 +18,12 @@ describe("EnvironmentLoaderProcessEnv", () => {
 
   test("failure", () => {
     expect(
-      () => new EnvironmentLoaderProcessEnvAdapter({ type: "invalid", schema: Schema }, process.env),
-    ).toThrow(NodeEnvironmentError.Invalid);
+      async () =>
+        await new EnvironmentLoaderProcessEnvAdapter(
+          // @ts-expect-error
+          { type: "invalid", schema: Schema },
+          { ...process.env, APP_NAME: 123 },
+        ).load(),
+    ).toThrow();
   });
 });
