5a09f4e034e05ee5d9fb5231abefd6e2f0db528a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-15 13:12:50 +0100

    step: add environment loader encrypted adapter tests

---
+++ tests/environment-loader-encrypted.adapter.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "bun:test";
+import * as tools from "@bgord/tools";
+import { z } from "zod/v4";
+import { EncryptionNoopAdapter } from "../src/encryption-noop.adapter";
+import { EnvironmentLoaderEncryptedAdapter } from "../src/environment-loader-encrypted.adapter";
+import { NodeEnvironmentEnum } from "../src/node-env.vo";
+
+const path = tools.FilePathRelative.fromString("config/secrets.txt");
+const env = new TextEncoder().encode("APP_NAME=MyApp").buffer;
+
+const config = { type: NodeEnvironmentEnum.local, Schema: z.object({ APP_NAME: z.string() }) };
+
+describe("EnvironmentLoaderProcess", () => {
+  test("happy path", async () => {
+    const result = await new EnvironmentLoaderEncryptedAdapter(config, path, {
+      Encryption: new EncryptionNoopAdapter(env),
+    }).load();
+
+    expect(result.APP_NAME).toEqual("MyApp");
+    expect(result.type).toEqual(NodeEnvironmentEnum.local);
+  });
+
+  test("failure", () => {
+    expect(
+      async () =>
+        await new EnvironmentLoaderEncryptedAdapter(config, path, {
+          Encryption: new EncryptionNoopAdapter(),
+        }).load(),
+    ).toThrow();
+  });
+});
