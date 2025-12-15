e5a98d788c156d67e0d68c8995b5efdec506e855
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-15 11:36:53 +0100

    step: add environment loader port

---
+++ src/environment-loader.port.ts
@@ -0,0 +1,8 @@
+import type { z } from "zod/v4";
+import type { NodeEnvironmentEnum } from "../src/node-env.vo";
+
+export type EnvironmentResultType<Schema> = z.infer<Schema> & { type: NodeEnvironmentEnum };
+
+export interface EnvironmentLoaderPort<Schema extends z.ZodObject<any>> {
+  load(): Promise<EnvironmentResultType<Schema>>;
+}
