8e3ac8a4cbff15273219749c6a46ec008019ef34
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-18 14:14:28 +0100

    step: cleanup rcs

--- release-candidates/index.ts
+++ release-candidates/index.ts
@@ -1,2 +1,3 @@
-export * from "./encryption.service";
-export * from "./gzip.service";
+export * from "./markdown-generator.port";
+export * from "./markdown-generator-marked.adapter";
+export * from "./markdown-generator-noop.adapter";
