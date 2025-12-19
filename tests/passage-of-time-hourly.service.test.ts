0c31e7fca42af5bbba222b4ebc11d5f8cfa1dbf4
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-19 17:17:39 +0100

    step: test job handler testcases

--- tests/passage-of-time-hourly.service.test.ts
+++ tests/passage-of-time-hourly.service.test.ts
@@ -2,19 +2,33 @@ import { describe, expect, spyOn, test } from "bun:test";
 import { ClockFixedAdapter } from "../src/clock-fixed.adapter";
 import { CorrelationStorage } from "../src/correlation-storage.service";
 import { IdProviderDeterministicAdapter } from "../src/id-provider-deterministic.adapter";
+import { JobHandlerBare } from "../src/job-handler-bare.adapter";
 import { PassageOfTimeHourly } from "../src/modules/system/services/passage-of-time-hourly.service";
 import * as mocks from "./mocks";
 
 const Clock = new ClockFixedAdapter(mocks.TIME_ZERO);
-const IdProvider = new IdProviderDeterministicAdapter([mocks.correlationId]);
-const deps = { Clock, IdProvider, EventStore: { save: async () => {}, saveAfter: async () => {} } };
-const service = new PassageOfTimeHourly(deps);
+const EventStore = { save: async () => {}, saveAfter: async () => {} };
 
 describe("PassageOfTimeHourly", async () => {
   test("correct path", async () => {
+    const IdProvider = new IdProviderDeterministicAdapter([mocks.correlationId, mocks.correlationId]);
+    const deps = { Clock, IdProvider, EventStore };
+    const service = new PassageOfTimeHourly(deps);
     const eventStoreSave = spyOn(deps.EventStore, "save");
 
     await CorrelationStorage.run(mocks.correlationId, async () => service.process());
+
+    expect(eventStoreSave).toHaveBeenCalledWith([mocks.GenericHourHasPassedEvent]);
+  });
+
+  test("job handler", async () => {
+    const IdProvider = new IdProviderDeterministicAdapter([mocks.correlationId, mocks.correlationId]);
+    const deps = { Clock, IdProvider, EventStore };
+    const JobHandler = new JobHandlerBare(deps);
+    const service = new PassageOfTimeHourly(deps);
+    const eventStoreSave = spyOn(deps.EventStore, "save");
+
+    await CorrelationStorage.run(mocks.correlationId, async () => JobHandler.handle(service)());
 
     expect(eventStoreSave).toHaveBeenCalledWith([mocks.GenericHourHasPassedEvent]);
   });
--- tests/passage-of-time-minute.service.test.ts
+++ tests/passage-of-time-minute.service.test.ts
@@ -2,19 +2,33 @@ import { describe, expect, spyOn, test } from "bun:test";
 import { ClockFixedAdapter } from "../src/clock-fixed.adapter";
 import { CorrelationStorage } from "../src/correlation-storage.service";
 import { IdProviderDeterministicAdapter } from "../src/id-provider-deterministic.adapter";
+import { JobHandlerBare } from "../src/job-handler-bare.adapter";
 import { PassageOfTimeMinute } from "../src/modules/system/services/passage-of-time-minute.service";
 import * as mocks from "./mocks";
 
 const Clock = new ClockFixedAdapter(mocks.TIME_ZERO);
-const IdProvider = new IdProviderDeterministicAdapter([mocks.correlationId]);
-const deps = { Clock, IdProvider, EventStore: { save: async () => {}, saveAfter: async () => {} } };
-const service = new PassageOfTimeMinute(deps);
+const EventStore = { save: async () => {}, saveAfter: async () => {} };
 
 describe("PassageOfTimeMinute", async () => {
   test("correct path", async () => {
+    const IdProvider = new IdProviderDeterministicAdapter([mocks.correlationId, mocks.correlationId]);
+    const deps = { Clock, IdProvider, EventStore };
+    const service = new PassageOfTimeMinute(deps);
     const eventStoreSave = spyOn(deps.EventStore, "save");
 
     await CorrelationStorage.run(mocks.correlationId, async () => service.process());
+
+    expect(eventStoreSave).toHaveBeenCalledWith([mocks.GenericMinuteHasPassedEvent]);
+  });
+
+  test("job handler", async () => {
+    const IdProvider = new IdProviderDeterministicAdapter([mocks.correlationId, mocks.correlationId]);
+    const deps = { Clock, IdProvider, EventStore };
+    const JobHandler = new JobHandlerBare(deps);
+    const service = new PassageOfTimeMinute(deps);
+    const eventStoreSave = spyOn(deps.EventStore, "save");
+
+    await CorrelationStorage.run(mocks.correlationId, async () => JobHandler.handle(service)());
 
     expect(eventStoreSave).toHaveBeenCalledWith([mocks.GenericMinuteHasPassedEvent]);
   });
