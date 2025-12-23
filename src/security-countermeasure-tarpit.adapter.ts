1c2e954eb85edcf1832353b876d881e9053fce29
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-23 18:14:53 +0100

    feature: implement SecurityCountermeasureTarpitAdapter

---
+++ src/security-countermeasure-tarpit.adapter.ts
@@ -0,0 +1,30 @@
+import type * as tools from "@bgord/tools";
+import { CorrelationStorage } from "./correlation-storage.service";
+import type { LoggerPort } from "./logger.port";
+import type { SecurityCountermeasurePort } from "./security-countermeasure.port";
+
+type Dependencies = { Logger: LoggerPort };
+
+export const SecurityCountermeasureTarpitAdapterError = {
+  Executed: "security.countermeasure.tarpit.adapter.executed",
+};
+
+export class SecurityCountermeasureTarpitAdapter implements SecurityCountermeasurePort {
+  constructor(
+    private readonly config: { delay: tools.Duration },
+    private readonly deps: Dependencies,
+  ) {}
+
+  async execute() {
+    this.deps.Logger.info({
+      message: "Security countermeasure tarpit",
+      component: "security",
+      operation: "security_countermeasure_tarpit",
+      correlationId: CorrelationStorage.get(),
+    });
+
+    await Bun.sleep(this.config.delay.ms);
+
+    throw new Error(SecurityCountermeasureTarpitAdapterError.Executed);
+  }
+}
---
+++ tests/security-countermeasure-tarpit.adapter.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, jest, spyOn, test } from "bun:test";
+import * as tools from "@bgord/tools";
+import { CorrelationStorage } from "../src/correlation-storage.service";
+import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+import {
+  SecurityCountermeasureTarpitAdapter,
+  SecurityCountermeasureTarpitAdapterError,
+} from "../src/security-countermeasure-tarpit.adapter";
+import * as mocks from "./mocks";
+
+const Logger = new LoggerNoopAdapter();
+const config = { delay: tools.Duration.Seconds(5) };
+const deps = { Logger };
+const countermeasure = new SecurityCountermeasureTarpitAdapter(config, deps);
+
+describe("SecurityCountermeasureTarpitAdapter", () => {
+  test("happy path", async () => {
+    const bunSleep = spyOn(Bun, "sleep").mockImplementation(jest.fn());
+    const loggerInfo = spyOn(Logger, "info");
+
+    await CorrelationStorage.run(mocks.correlationId, async () => {
+      expect(async () => countermeasure.execute()).toThrow(SecurityCountermeasureTarpitAdapterError.Executed);
+    });
+
+    expect(bunSleep).toHaveBeenCalledWith(config.delay.ms);
+    expect(loggerInfo).toHaveBeenCalledWith({
+      message: "Security countermeasure tarpit",
+      component: "security",
+      operation: "security_countermeasure_tarpit",
+      correlationId: mocks.correlationId,
+    });
+  });
+});
