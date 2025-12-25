cde91067e9627b9b0a3056259e907887ecd3d34f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-26 00:07:49 +0100

    step: wire up decorators to prerequisite vo

--- src/prerequisite.vo.ts
+++ src/prerequisite.vo.ts
@@ -1,3 +1,4 @@
+import type { PrerequisiteVerifierDecorator } from "./prerequisite-verifier.decorator";
 import type { PrerequisiteVerifierPort } from "./prerequisite-verifier.port";
 
 export type PrerequisiteLabelType = string;
@@ -6,6 +7,7 @@ export class Prerequisite {
   constructor(
     readonly label: PrerequisiteLabelType,
     readonly verifier: PrerequisiteVerifierPort,
+    readonly decorators: PrerequisiteVerifierDecorator[] = [],
     readonly enabled: boolean = true,
   ) {}
 }
