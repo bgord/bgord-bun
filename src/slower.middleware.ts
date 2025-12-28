1f493c8792752cca5afba7e00454aa2693aac3c2
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-28 13:21:17 +0100

    refactor: use sleeper port in slower

--- src/slower.middleware.ts
+++ src/slower.middleware.ts
@@ -1,10 +1,13 @@
 import type * as tools from "@bgord/tools";
 import { createMiddleware } from "hono/factory";
+import type { SleeperPort } from "./sleeper.port";
+
+type Dependencies = { Sleeper: SleeperPort };
 
 export class Slower {
-  static handle = (offset: tools.Duration) =>
+  static handle = (offset: tools.Duration, deps: Dependencies) =>
     createMiddleware(async (_c, next) => {
-      await Bun.sleep(offset.ms);
+      await deps.Sleeper.wait(offset);
 
       return next();
     });
--- tests/slower.middleware.test.ts
+++ tests/slower.middleware.test.ts
@@ -1,17 +1,21 @@
-import { describe, expect, jest, spyOn, test } from "bun:test";
+import { describe, expect, spyOn, test } from "bun:test";
 import * as tools from "@bgord/tools";
 import { Hono } from "hono";
+import { SleeperNoopAdapter } from "../src/sleeper-noop.adapter";
 import { Slower } from "../src/slower.middleware";
+
+const Sleeper = new SleeperNoopAdapter();
+const deps = { Sleeper };
 
 describe("Slower middleware", () => {
   test("happy path", async () => {
     const duration = tools.Duration.Seconds(3);
-    const bunSleep = spyOn(Bun, "sleep").mockImplementation(jest.fn());
-    const app = new Hono().get("/slower", Slower.handle(duration), (c) => c.text("OK"));
+    const sleeperWait = spyOn(Sleeper, "wait");
+    const app = new Hono().get("/slower", Slower.handle(duration, deps), (c) => c.text("OK"));
 
     const response = await app.request("/slower");
 
     expect(await response.text()).toEqual("OK");
-    expect(bunSleep).toHaveBeenCalledWith(duration.ms);
+    expect(sleeperWait).toHaveBeenCalledWith(duration);
   });
 });
