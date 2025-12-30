415c1ee40e84149119660f9535cbe9fa72cc614f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-30 14:04:41 +0100

    refactor: use positive integer in countermeasure strategy

--- src/security-countermeasure.strategy.ts
+++ src/security-countermeasure.strategy.ts
@@ -1,10 +1,11 @@
 import type * as tools from "@bgord/tools";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
 import type { SecurityContext } from "./security-context.vo";
 import type { SecurityCountermeasureNameType } from "./security-countermeasure-name.vo";
 
 export type SecurityAction =
   | { kind: "allow" }
-  | { kind: "deny"; reason: string; response: { status: number } }
+  | { kind: "deny"; reason: string; response: { status: ContentfulStatusCode } }
   | { kind: "delay"; duration: tools.Duration; after: SecurityAction }
   | { kind: "mirage"; response: { status: number } };
 
