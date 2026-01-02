658c7930429810049aa2f5c0d712feaabba0799d
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-02 20:11:22 +0100

    step: pdf generator noop

--- tests/pdf-generator-noop.adapter.test.ts
+++ tests/pdf-generator-noop.adapter.test.ts
@@ -1,13 +1,16 @@
 import { describe, expect, spyOn, test } from "bun:test";
 import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
-import { PdfGeneratorNoopAdapter, PLACEHOLDER_PDF_BASE64 } from "../src/pdf-generator-noop.adapter";
+import { PdfGeneratorNoopAdapter } from "../src/pdf-generator-noop.adapter";
+
+const PLACEHOLDER_PDF_BASE64 =
+  "JVBERi0xLjQKMSAwIG9iago8PC9UeXBlIC9DYXRhbG9nCi9QYWdlcyAyIDAgUgo+PgplbmRvYmoK MiAwIG9iago8PC9UeXBlIC9QYWdlcwovS2lkcyBbMyAwIFJdCi9Db3VudCAxCj4+CmVuZG9iagoz IDAgb2JqCjw8L1R5cGUgL1BhZ2UKL1BhcmVudCAyIDAgUgovTWVkaWFCb3ggWzAgMCA1OTUgODQy XQovQ29udGVudHMgNSAwIFIKL1Jlc291cmNlcyA8PC9Qcm9jU2V0IFsvUERGIC9UZXh0XQovRm9u dCA8PC9GMSA0IDAgUj4+Cj4+Cj4+CmVuZG9iago0IDAgb2JqCjw8L1R5cGUgL0ZvbnQKL1N1YnR5 cGUgL1R5cGUxCi9OYW1lIC9GMQovQmFzZUZvbnQgL0hlbHZldGljYQovRW5jb2RpbmcgL01hY1Jv bWFuRW5jb2RpbmcKPj4KZW5kb2JqCjUgMCBvYmoKPDwvTGVuZ3RoIDUzCj4+CnN0cmVhbQpCVAov RjEgMjAgVGYKMjIwIDQwMCBUZAooRHVtbXkgUERGKSBUagpFVAplbmRzdHJlYW0KZW5kb2JqCnhy ZWYKMCA2CjAwMDAwMDAwMDAgNjU1MzUgZgowMDAwMDAwMDA5IDAwMDAwIG4KMDAwMDAwMDA2MyAw MDAwMCBuCjAwMDAwMDAxMjQgMDAwMDAgbgowMDAwMDAwMjc3IDAwMDAwIG4KMDAwMDAwMDM5MiAw MDAwMCBuCnRyYWlsZXIKPDwvU2l6ZSA2Ci9Sb290IDEgMCBSCj4+CnN0YXJ0eHJlZgo0OTUKJSVF T0YK";
 
 const Logger = new LoggerNoopAdapter();
 const deps = { Logger };
 
 const adapter = new PdfGeneratorNoopAdapter(deps);
 
-describe("CertificateInspectorNoopAdapter", () => {
+describe("PdfGeneratorNoopAdapter", () => {
   test("success", async () => {
     const loggerInfo = spyOn(Logger, "info");
 
