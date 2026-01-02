f5bec11162a3dc4f7abf974637b466cb2ad96d9f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-02 20:08:16 +0100

    step: checksum

--- src/checksum.service.ts
+++ src/checksum.service.ts
@@ -4,7 +4,7 @@ export class Checksum {
   static compare(first: HashFileResult, second: HashFileResult): boolean {
     return (
       first.etag.matches(second.etag) &&
-      first.size === second.size &&
+      first.size.toBytes() === second.size.toBytes() &&
       first.lastModified &&
       second.lastModified &&
       first.mime.isSatisfiedBy(second.mime)
