50fa0e03c9fc7a4b86eeae19803725ac20bc6cd3
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-02 20:20:24 +0100

    step: mailer smtp with logger adapter

--- tests/mailer-smtp-with-logger-adapter.test.ts
+++ tests/mailer-smtp-with-logger-adapter.test.ts
@@ -3,6 +3,7 @@ import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
 import { SmtpPort } from "../src/mailer.vo";
 import { MailerSmtpAdapter } from "../src/mailer-smtp.adapter";
 import { MailerSmtpWithLoggerAdapter } from "../src/mailer-smtp-with-logger.adapter";
+import * as mocks from "./mocks";
 
 const Logger = new LoggerNoopAdapter();
 const MailerSmtp = new MailerSmtpAdapter({
@@ -29,6 +30,34 @@ describe("SmtpMailerWithLoggerAdapter", () => {
     await mailer.send(sendOptions);
 
     expect(sendMail).toHaveBeenCalledWith(sendOptions);
-    expect(loggerInfo).toHaveBeenCalledTimes(2);
+    expect(loggerInfo).toHaveBeenNthCalledWith(1, {
+      component: "infra",
+      message: "Mailer attempt",
+      metadata: sendOptions,
+      operation: "mailer",
+    });
+    expect(loggerInfo).toHaveBeenNthCalledWith(2, {
+      component: "infra",
+      message: "Mailer success",
+      metadata: { message: sendOptions },
+      operation: "mailer",
+    });
+  });
+
+  test("failure", async () => {
+    const sendMail = spyOn(MailerSmtp, "send").mockImplementation(mocks.throwIntentionalError);
+    const loggerError = spyOn(Logger, "error");
+    const sendOptions = {
+      from: "sender@example.com",
+      to: "recipient@example.com",
+      subject: "Test Email",
+      text: "This is a test email.",
+    };
+
+    expect(async () => mailer.send(sendOptions)).toThrow(mocks.IntentionalError);
+    expect(sendMail).toHaveBeenCalledWith(sendOptions);
+    expect(loggerError).toHaveBeenCalledWith(
+      expect.objectContaining({ component: "infra", message: "Mailer error", operation: "mailer" }),
+    );
   });
 });
