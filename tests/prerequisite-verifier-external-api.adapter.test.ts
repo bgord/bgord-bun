3cce4dc3055f281985d755b07610f457841f6255
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-02 16:58:24 +0100

    step: prereq external api

--- tests/prerequisite-verifier-external-api.adapter.test.ts
+++ tests/prerequisite-verifier-external-api.adapter.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, spyOn, test } from "bun:test";
+import { PrerequisiteVerificationOutcome } from "../src/prerequisite-verifier.port";
 import { PrerequisiteVerifierExternalApiAdapter } from "../src/prerequisite-verifier-external-api.adapter";
 import * as mocks from "./mocks";
 
@@ -11,10 +12,19 @@ describe("PrerequisiteVerifierExternalApiAdapter", () => {
     expect(await prerequisite.verify()).toEqual(mocks.VerificationSuccess);
   });
 
-  test("failure", async () => {
+  test("failure - response not ok", async () => {
     spyOn(global, "fetch").mockResolvedValue({ ok: false, status: 400 } as any);
 
     expect(await prerequisite.verify()).toEqual(mocks.VerificationFailure({ message: "HTTP 400" }));
+  });
+
+  test("failure - response error", async () => {
+    // @ts-expect-error
+    spyOn(global, "fetch").mockImplementation(mocks.throwIntentionalErrorAsync);
+
+    const result = await prerequisite.verify();
+
+    expect(result.outcome).toEqual(PrerequisiteVerificationOutcome.failure);
   });
 
   test("kind", () => {
