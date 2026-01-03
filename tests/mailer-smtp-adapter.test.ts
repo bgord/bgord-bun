30e1dc007fa9734b6bb8e8938a26b70bccbf44b0
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-03 20:31:31 +0100

    step: mailer smtp adapter

--- tests/mailer-smtp-adapter.test.ts
+++ tests/mailer-smtp-adapter.test.ts
@@ -3,16 +3,20 @@ import nodemailer from "nodemailer";
 import { SmtpPort } from "../src/mailer.vo";
 import { MailerSmtpAdapter } from "../src/mailer-smtp.adapter";
 
+const smtp = {
+  SMTP_HOST: "smtp.example.com",
+  SMTP_PORT: SmtpPort.parse(587),
+  SMTP_USER: "user@example.com",
+  SMTP_PASS: "password",
+};
+
 describe("MailerSmtpAdapter", () => {
   test("send - success", async () => {
     const sendMail = jest.fn();
-    spyOn(nodemailer, "createTransport").mockReturnValue({ sendMail } as any);
-    const mailer = new MailerSmtpAdapter({
-      SMTP_HOST: "smtp.example.com",
-      SMTP_PORT: SmtpPort.parse(587),
-      SMTP_USER: "user@example.com",
-      SMTP_PASS: "password",
-    });
+    const nodemailerCreateTransport = spyOn(nodemailer, "createTransport").mockReturnValue({
+      sendMail,
+    } as any);
+    const mailer = new MailerSmtpAdapter(smtp);
     const sendOptions = {
       from: "sender@example.com",
       to: "recipient@example.com",
@@ -23,17 +27,17 @@ describe("MailerSmtpAdapter", () => {
     await mailer.send(sendOptions);
 
     expect(sendMail).toHaveBeenCalledWith(sendOptions);
+    expect(nodemailerCreateTransport).toHaveBeenCalledWith({
+      auth: { user: smtp.SMTP_USER, pass: smtp.SMTP_PASS },
+      host: smtp.SMTP_HOST,
+      port: smtp.SMTP_PORT,
+    });
   });
 
   test("verify - success", async () => {
     const verify = jest.fn();
     spyOn(nodemailer, "createTransport").mockImplementation(() => ({ verify }) as any);
-    const mailer = new MailerSmtpAdapter({
-      SMTP_HOST: "smtp.example.com",
-      SMTP_PORT: SmtpPort.parse(587),
-      SMTP_USER: "user@example.com",
-      SMTP_PASS: "password",
-    });
+    const mailer = new MailerSmtpAdapter(smtp);
 
     await mailer.verify();
 
