82278fb0ba7723760d65d647e2dd0dcf4713a076
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-03 23:32:34 +0100

    step: file uploader

--- tests/file-uploader.middleware.test.ts
+++ tests/file-uploader.middleware.test.ts
@@ -7,7 +7,12 @@ const boundary = "----bun-test-boundary";
 const headers = { "Content-Type": `multipart/form-data; boundary=${boundary}` };
 
 const app = new Hono()
-  .use(...FileUploader.validate({ mimeTypes: [tools.MIMES.png], maxFilesSize: tools.Size.fromKb(10) }))
+  .use(
+    ...FileUploader.validate({
+      mimeTypes: [tools.MIMES.png, tools.MIMES.csv],
+      maxFilesSize: tools.Size.fromKb(10),
+    }),
+  )
   .post("/uploader", (c) => c.text("uploaded"));
 
 describe("FileUploader middleware", () => {
@@ -52,7 +57,7 @@ describe("FileUploader middleware", () => {
     const result = await response.text();
 
     expect(response.status).toEqual(400);
-    expect(result).toEqual(InvalidFileMimeTypeError.message);
+    expect(result).toEqual("invalid_file_mime_type_error");
   });
 
   test("rejects file too big", async () => {
@@ -77,6 +82,29 @@ describe("FileUploader middleware", () => {
     const result = await response.text();
 
     expect(response.status).toEqual(400);
-    expect(result).toEqual(FileTooBigError.message);
+    expect(result).toEqual("file_too_big_error");
+  });
+
+  test("rejects no file", async () => {
+    const content = [
+      `--${boundary}`,
+      'Content-Disposition: form-data; name="file"',
+      "",
+      "invalid",
+      `--${boundary}--`,
+      "",
+    ].join("\r\n");
+    const app = new Hono()
+      .use(...FileUploader.validate({ mimeTypes: [tools.MIMES.png], maxFilesSize: tools.Size.fromKb(10) }))
+      .post("/uploader", (c) => c.text("uploaded"));
+
+    const response = await app.request("/uploader", {
+      method: "POST",
+      body: new TextEncoder().encode(content),
+      headers,
+    });
+
+    expect(response.status).toEqual(400);
+    expect(await response.text()).toEqual("invalid_file_mime_type_error");
   });
 });
