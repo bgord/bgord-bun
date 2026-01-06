60ddc7092210b4e8481f5886b3ea40d4db7941ba
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-07 00:54:50 +0100

    refactor: use noop build in api version tests

--- tests/api-version-middleware.test.ts
+++ tests/api-version-middleware.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, spyOn, test } from "bun:test";
 import * as tools from "@bgord/tools";
 import { Hono } from "hono";
 import { ApiVersion } from "../src/api-version.middleware";
-import { BuildInfoRepositoryPackageJsonStrategy } from "../src/build-info-repository-package-json.strategy";
+import { BuildInfoRepositoryNoopStrategy } from "../src/build-info-repository-noop.strategy";
 import { CacheRepositoryNodeCacheAdapter } from "../src/cache-repository-node-cache.adapter";
 import { CacheResolverSimpleStrategy } from "../src/cache-resolver-simple.strategy";
 import { CacheSubjectApplicationResolver } from "../src/cache-subject-application-resolver.vo";
@@ -10,23 +10,20 @@ import { CacheSubjectSegmentFixedStrategy } from "../src/cache-subject-segment-f
 import { ClockSystemAdapter } from "../src/clock-system.adapter";
 import { FileReaderJsonNoopAdapter } from "../src/file-reader-json-noop.adapter";
 import { HashContentSha256BunStrategy } from "../src/hash-content-sha256-bun.strategy";
+import * as mocks from "./mocks";
 
-const BUILD_DATE = tools.Timestamp.fromNumber(123);
+const version = "1.2.3";
 
 const Clock = new ClockSystemAdapter();
-const FileReaderJson = new FileReaderJsonNoopAdapter({});
 const config = { type: "infinite" } as const;
 const CacheRepository = new CacheRepositoryNodeCacheAdapter(config);
 const CacheResolver = new CacheResolverSimpleStrategy({ CacheRepository });
 const HashContent = new HashContentSha256BunStrategy();
-const BuildInfoRepository = new BuildInfoRepositoryPackageJsonStrategy({ Clock, FileReaderJson });
-const deps = {
-  Clock,
-  FileReaderJson,
-  CacheResolver,
-  HashContent,
-  BuildInfoRepository,
-};
+const BuildInfoRepository = new BuildInfoRepositoryNoopStrategy(
+  mocks.TIME_ZERO,
+  tools.PackageVersion.fromString(version),
+);
+const deps = { Clock, CacheResolver, HashContent, BuildInfoRepository };
 const app = new Hono().use(ApiVersion.build(deps)).get("/ping", (c) => c.text("OK"));
 
 describe("ApiVersion middleware", async () => {
@@ -37,22 +34,19 @@ describe("ApiVersion middleware", async () => {
   const subject = await resolver.resolve();
 
   test("happy path", async () => {
-    const buildInfoRepositoryExtract = spyOn(deps.BuildInfoRepository, "extract").mockResolvedValue({
-      BUILD_DATE,
-      BUILD_VERSION: tools.PackageVersion.fromString("1.0.0"),
-    });
+    const buildInfoRepositoryExtract = spyOn(deps.BuildInfoRepository, "extract");
     const getSpy = spyOn(CacheRepository, "get");
 
     const first = await app.request("/ping", { method: "GET" });
 
     expect(first.status).toEqual(200);
-    expect(first.headers.get(ApiVersion.HEADER_NAME)).toEqual("1.0.0");
+    expect(first.headers.get(ApiVersion.HEADER_NAME)).toEqual(version);
     expect(getSpy).toHaveBeenCalledWith(subject.hex);
 
     const second = await app.request("/ping", { method: "GET" });
 
     expect(second.status).toEqual(200);
-    expect(second.headers.get(ApiVersion.HEADER_NAME)).toEqual("1.0.0");
+    expect(second.headers.get(ApiVersion.HEADER_NAME)).toEqual(version);
     expect(buildInfoRepositoryExtract).toBeCalledTimes(1);
     expect(getSpy).toHaveBeenCalledWith(subject.hex);
 
@@ -66,13 +60,10 @@ describe("ApiVersion middleware", async () => {
     const CacheRepository = new CacheRepositoryNodeCacheAdapter(config);
     const CacheResolver = new CacheResolverSimpleStrategy({ CacheRepository });
     const HashContent = new HashContentSha256BunStrategy();
-    const BuildInfoRepository = new BuildInfoRepositoryPackageJsonStrategy({ Clock, FileReaderJson });
+    const BuildInfoRepository = new BuildInfoRepositoryNoopStrategy(mocks.TIME_ZERO);
     const deps = { Clock, FileReaderJson, CacheResolver, HashContent, BuildInfoRepository };
     const app = new Hono().use(ApiVersion.build(deps)).get("/ping", (c) => c.text("OK"));
-    const buildInfoRepositoryExtract = spyOn(deps.BuildInfoRepository, "extract").mockResolvedValue({
-      BUILD_DATE,
-      BUILD_VERSION: undefined,
-    });
+    const buildInfoRepositoryExtract = spyOn(deps.BuildInfoRepository, "extract");
     const getSpy = spyOn(CacheRepository, "get");
 
     const first = await app.request("/ping", { method: "GET" });
