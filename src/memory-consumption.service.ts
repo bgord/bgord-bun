a1bb53f6566482803ab85fa024d6d8aa73cd5288
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-15 23:05:52 +0100

    step: add memory consumption snapshot

--- src/memory-consumption.service.ts
+++ src/memory-consumption.service.ts
@@ -1,7 +1,23 @@
 import * as tools from "@bgord/tools";
 
+type MemoryConsumptionSnapshotType = { total: tools.Size; heap: { used: tools.Size; total: tools.Size } };
+
+// RSS - resident set size - how much memory this process uses
+// Heap total - how much memory is reserved for the JS heap
+// Heap used - how much of the reserved memory is actually used for the JS heap
+
 export class MemoryConsumption {
   static get(): tools.Size {
     return tools.Size.fromBytes(process.memoryUsage().rss);
+  }
+
+  static snapshot(): MemoryConsumptionSnapshotType {
+    return {
+      total: tools.Size.fromBytes(process.memoryUsage().rss),
+      heap: {
+        used: tools.Size.fromBytes(process.memoryUsage().heapUsed),
+        total: tools.Size.fromBytes(process.memoryUsage().heapTotal),
+      },
+    };
   }
 }
--- tests/memory-consumption.service.test.ts
+++ tests/memory-consumption.service.test.ts
@@ -3,7 +3,14 @@ import * as tools from "@bgord/tools";
 import { MemoryConsumption } from "../src/memory-consumption.service";
 
 const rss = tools.Size.fromBytes(123456789);
-const memoryUsage = { rss: rss.toBytes() } as any;
+const heapUsed = tools.Size.fromBytes(234567890);
+const heapTotal = tools.Size.fromBytes(345678901);
+
+const memoryUsage = {
+  rss: rss.toBytes(),
+  heapUsed: heapUsed.toBytes(),
+  heapTotal: heapTotal.toBytes(),
+} as any;
 
 describe("MemoryConsumption service", () => {
   test("get", () => {
@@ -13,5 +20,15 @@ describe("MemoryConsumption service", () => {
 
     expect(result.equals(rss)).toEqual(true);
     expect(result.format(tools.Size.unit.MB)).toEqual("117.74 MB");
+  });
+
+  test("snapshot", () => {
+    spyOn(process, "memoryUsage").mockReturnValue(memoryUsage);
+
+    const result = MemoryConsumption.snapshot();
+
+    expect(result.total.equals(rss)).toEqual(true);
+    expect(result.heap.total.equals(heapTotal)).toEqual(true);
+    expect(result.heap.used.equals(heapUsed)).toEqual(true);
   });
 });
