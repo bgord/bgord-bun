560bd860b90aca0c017a8dc580b0ee7d13c6e94e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-17 16:43:57 +0100

    refactor: use mime registry in hash file sha 256 bun adapter

--- src/hash-file-sha256-bun.adapter.ts
+++ src/hash-file-sha256-bun.adapter.ts
@@ -2,22 +2,26 @@ import * as tools from "@bgord/tools";
 import type { HashContentStrategy } from "./hash-content.strategy";
 import type { HashFilePort } from "./hash-file.port";
 
-type Dependencies = { HashContent: HashContentStrategy };
+type Dependencies = { HashContent: HashContentStrategy; MimeRegistry: tools.MimeRegistry };
 
 export class HashFileSha256BunAdapter implements HashFilePort {
   constructor(private readonly deps: Dependencies) {}
 
   async hash(path: tools.FilePathAbsolute | tools.FilePathRelative) {
-    const file = Bun.file(path.get());
     const extension = path.getFilename().getExtension();
 
+    const mime = this.deps.MimeRegistry.fromExtension(extension);
+
+    if (!mime) throw new Error(tools.MimeRegistryError.MimeNotFound);
+
+    const file = Bun.file(path.get());
     const arrayBuffer = await file.arrayBuffer();
 
     return {
       etag: await this.deps.HashContent.hash(await file.text()),
       size: tools.Size.fromBytes(arrayBuffer.byteLength),
       lastModified: tools.Timestamp.fromNumber(file.lastModified),
-      mime: tools.Mime.fromExtension(extension),
+      mime,
     };
   }
 }
--- tests/hash-file-sha256-bun.adapter.test.ts
+++ tests/hash-file-sha256-bun.adapter.test.ts
@@ -4,55 +4,71 @@ import { Hash } from "../src/hash.vo";
 import { HashContentSha256BunStrategy } from "../src/hash-content-sha256-bun.strategy";
 import { HashFileSha256BunAdapter } from "../src/hash-file-sha256-bun.adapter";
 
+const jpegMime = tools.Mime.fromString("image/jpeg");
+const jpgExtension = tools.Extension.parse("jpg");
+const jpegExtension = tools.Extension.parse("jpeg");
+
+const MimeRegistry = new tools.MimeRegistry([{ mime: jpegMime, extensions: [jpgExtension, jpegExtension] }]);
+
 const HashContent = new HashContentSha256BunStrategy();
-const deps = { HashContent };
+const deps = { HashContent, MimeRegistry };
 
 const adapter = new HashFileSha256BunAdapter(deps);
 
 describe("HashFileSha256BunAdapter", () => {
   test("absolute path", async () => {
     const text = "hello";
-    const fakeFile = {
+    const file = {
       arrayBuffer: async () => new TextEncoder().encode(text).buffer,
       text: async () => text,
       lastModified: 0,
-    };
-    // @ts-expect-error
-    const bunFile = spyOn(Bun, "file").mockImplementation(() => fakeFile);
-    const input = tools.FilePathAbsolute.fromString("/var/data/hello.pdf");
+    } as any;
+    const bunFile = spyOn(Bun, "file").mockImplementation(() => file);
+    const input = tools.FilePathAbsolute.fromString("/var/data/hello.jpg");
 
     const result = await adapter.hash(input);
 
-    expect(bunFile).toHaveBeenCalledWith("/var/data/hello.pdf");
+    expect(bunFile).toHaveBeenCalledWith("/var/data/hello.jpg");
     expect(result.etag).toEqual(
       Hash.fromString("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"),
     );
     // @ts-expect-error
     expect(result.size.toBytes()).toEqual(5);
     expect(result.lastModified).toEqual(tools.Timestamp.fromNumber(0));
-    expect(result.mime.toString()).toEqual("application/pdf");
+    expect(result.mime.toString()).toEqual("image/jpeg");
+  });
+
+  test("absolute path - mime not found", async () => {
+    const input = tools.FilePathAbsolute.fromString("/var/data/hello.pdf");
+
+    expect(async () => adapter.hash(input)).toThrow(tools.MimeRegistryError.MimeNotFound);
   });
 
   test("relative path", async () => {
     const text = "abc";
-    const fakeFile = {
+    const file = {
       arrayBuffer: async () => new TextEncoder().encode(text).buffer,
       text: async () => text,
       lastModified: 0,
-    };
-    // @ts-expect-error
-    const bunFile = spyOn(Bun, "file").mockImplementation(() => fakeFile);
-    const input = tools.FilePathRelative.fromString("images/payload.bin");
+    } as any;
+    const bunFile = spyOn(Bun, "file").mockImplementation(() => file);
+    const input = tools.FilePathRelative.fromString("images/payload.jpeg");
 
     const result = await adapter.hash(input);
 
-    expect(bunFile).toHaveBeenCalledWith("images/payload.bin");
+    expect(bunFile).toHaveBeenCalledWith("images/payload.jpeg");
     expect(result.etag).toEqual(
       Hash.fromString("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),
     );
     // @ts-expect-error
     expect(result.size.toBytes()).toEqual(3);
     expect(result.lastModified).toEqual(tools.Timestamp.fromNumber(0));
-    expect(result.mime.toString()).toEqual("application/octet-stream");
+    expect(result.mime.toString()).toEqual("image/jpeg");
+  });
+
+  test("relative path - mime not found", async () => {
+    const input = tools.FilePathRelative.fromString("images/payload.pdf");
+
+    expect(async () => adapter.hash(input)).toThrow(tools.MimeRegistryError.MimeNotFound);
   });
 });
