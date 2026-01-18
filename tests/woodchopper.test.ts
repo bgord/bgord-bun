a7820ac7050f4166dbe5b86e030bbfad11eccbcd
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-18 18:08:08 +0100

    step: define clock dependency

--- tests/woodchopper.test.ts
+++ tests/woodchopper.test.ts
@@ -1,12 +1,17 @@
 import { describe, expect, test } from "bun:test";
+import { ClockFixedAdapter } from "../src/clock-fixed.adapter";
 import { LogLevelEnum } from "../src/logger.port";
 import { NodeEnvironmentEnum } from "../src/node-env.vo";
 import { Woodchopper } from "../src/woodchopper";
+import * as mocks from "./mocks";
+
+const Clock = new ClockFixedAdapter(mocks.TIME_ZERO);
+const deps = { Clock };
 
 describe("Woodchopper", () => {
   test("constructor", () => {
     const config = { app: "woodchopper", level: LogLevelEnum.info, environment: NodeEnvironmentEnum.local };
 
-    expect(() => new Woodchopper(config)).not.toThrow();
+    expect(() => new Woodchopper(config, deps)).not.toThrow();
   });
 });
