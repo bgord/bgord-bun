0627242d682ce491f1b72dd952e4cddd3765688f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-21 09:28:31 +0100

    step: add redactor error hide stack

---
+++ src/redactor-error-stack-hide.strategy.ts
@@ -0,0 +1,26 @@
+import type { RedactorStrategy } from "./redactor.strategy";
+import type { NormalizedError } from "./error-normalizer.service";
+
+export class RedactorErrorStackHideStrategy implements RedactorStrategy {
+  redact<T>(input: T): T {
+    if (!this.isNormalizedError(input)) return input;
+    return this.hide(input) as T;
+  }
+
+  private hide(error: NormalizedError): NormalizedError {
+    return {
+      message: error.message,
+      name: error.name,
+      cause: error.cause ? this.hide(error.cause) : undefined,
+    };
+  }
+
+  private isNormalizedError(value: unknown): value is NormalizedError {
+    return (
+      typeof value === "object" &&
+      value !== null &&
+      "message" in value &&
+      typeof (value as any).message === "string"
+    );
+  }
+}
