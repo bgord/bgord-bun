a6d7645d3ee8e28341f4332401230475d8e201d3
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-21 10:09:17 +0100

    step: add optional redactor to diagnostics console error

--- src/woodchopper-diagnostics-console-error.strategy.ts
+++ src/woodchopper-diagnostics-console-error.strategy.ts
@@ -1,12 +1,17 @@
 import { ErrorNormalizer } from "./error-normalizer.service";
+import type { RedactorStrategy } from "./redactor.strategy";
 import type {
   WoodchopperDiagnosticsStrategy,
   WoodchopperDiagnosticType,
 } from "./woodchopper-diagnostics.strategy";
 
 export class WoodchopperDiagnosticsConsoleError implements WoodchopperDiagnosticsStrategy {
+  constructor(private readonly redactor?: RedactorStrategy) {}
+
   handle(diagnostic: WoodchopperDiagnosticType) {
+    const error = ErrorNormalizer.normalize(diagnostic.error);
+
     // biome-ignore lint: lint/suspicious/noConsole
-    console.error({ ...diagnostic, error: ErrorNormalizer.normalize(diagnostic.error) });
+    console.error({ ...diagnostic, error: this.redactor ? this.redactor.redact(error) : error });
   }
 }
--- tests/woodchopper-diagnostics-console-error.strategy.test.ts
+++ tests/woodchopper-diagnostics-console-error.strategy.test.ts
@@ -1,4 +1,7 @@
 import { describe, expect, jest, spyOn, test } from "bun:test";
+import { RedactorCompositeStrategy } from "../src/redactor-composite.strategy";
+import { RedactorErrorCauseDepthLimitStrategy } from "../src/redactor-error-cause-depth-limit.strategy";
+import { RedactorErrorStackHideStrategy } from "../src/redactor-error-stack-hide.strategy";
 import { WoodchopperDiagnosticsConsoleError } from "../src/woodchopper-diagnostics-console-error.strategy";
 import * as mocks from "./mocks";
 
@@ -12,6 +15,33 @@ describe("WoodchopperDiagnosticsConsoleError", () => {
     expect(consoleError).toHaveBeenCalledWith({
       kind: "sink",
       error: { message: mocks.IntentionalError, name: "Error", cause: undefined, stack: expect.any(String) },
+    });
+  });
+
+  test("handle - redactor", () => {
+    const consoleError = spyOn(console, "error").mockImplementation(jest.fn());
+    const redactor = new RedactorCompositeStrategy([
+      new RedactorErrorStackHideStrategy(),
+      new RedactorErrorCauseDepthLimitStrategy(1),
+    ]);
+    const diagnostics = new WoodchopperDiagnosticsConsoleError(redactor);
+
+    const error = new Error(mocks.IntentionalError);
+    const first = new Error("intentional.cause");
+    const second = new Error("intentional.cause");
+
+    error.cause = first;
+    first.cause = second;
+
+    diagnostics.handle({ kind: "sink", error });
+
+    expect(consoleError).toHaveBeenCalledWith({
+      kind: "sink",
+      error: {
+        message: mocks.IntentionalError,
+        name: "Error",
+        cause: { name: "Error", cause: undefined, message: "intentional.cause" },
+      },
     });
   });
 });
