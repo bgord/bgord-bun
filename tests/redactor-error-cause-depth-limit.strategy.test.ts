68b160d5934a25355ee65547c95efece5f8e02c6
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-21 11:54:47 +0100

    refactor: handle top level error in redactor error cause depth limit

--- tests/redactor-error-cause-depth-limit.strategy.test.ts
+++ tests/redactor-error-cause-depth-limit.strategy.test.ts
@@ -1,35 +1,42 @@
 import { describe, expect, test } from "bun:test";
-import type { NormalizedError } from "../src/error-normalizer.service";
+import { ErrorNormalizer } from "../src/error-normalizer.service";
 import { RedactorErrorCauseDepthLimitStrategy } from "../src/redactor-error-cause-depth-limit.strategy";
+import * as mocks from "./mocks";
 
 const redactor = new RedactorErrorCauseDepthLimitStrategy(1);
 
 describe("RedactorLimitErrorCauseDepthStrategy", () => {
   test("limit - above", () => {
-    const input: NormalizedError = {
-      message: "level 0",
-      name: "Error",
-      stack: "stack0",
-      cause: { message: "level 1", cause: { message: "level 2" } },
-    };
+    const IntentionalCause = "intentional.cause";
+    const cause = ErrorNormalizer.normalize(new Error(IntentionalCause));
+    const error = ErrorNormalizer.normalize(new Error(mocks.IntentionalError));
+    error.cause = cause;
 
-    expect(redactor.redact(input)).toEqual({
-      message: "level 0",
-      name: "Error",
-      stack: "stack0",
-      cause: { message: "level 1" },
+    expect(redactor.redact({ error })).toEqual({
+      error: {
+        message: mocks.IntentionalError,
+        name: "Error",
+        stack: expect.any(String),
+        cause: { message: IntentionalCause, name: "Error", stack: expect.any(String), cause: undefined },
+      },
     });
   });
 
   test("limit - below", () => {
-    const input: NormalizedError = { message: "root", cause: { message: "child" } };
+    const error = ErrorNormalizer.normalize(new Error(mocks.IntentionalError));
+
+    expect(redactor.redact({ error })).toEqual({ error });
+  });
+
+  test("noop - not plain object", () => {
+    const input = 5;
 
     expect(redactor.redact(input)).toEqual(input);
   });
 
-  test("noop", () => {
-    const value = { foo: "bar" };
+  test("noop - no error property", () => {
+    const input = { message: "message" };
 
-    expect(redactor.redact(value)).toEqual(value);
+    expect(redactor.redact(input)).toEqual(input);
   });
 });
