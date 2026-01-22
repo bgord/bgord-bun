bcc3112822c389a6f88d606a8f091c549c05852b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-22 09:56:57 +0100

    refactor: simplify client ua vo

--- src/client-user-agent.vo.ts
+++ src/client-user-agent.vo.ts
@@ -2,16 +2,17 @@ import * as z from "zod/v4";
 
 export const ClientUserAgentError = {
   Type: "client.user.agent.type",
-  Empty: "client.user.agent.empty",
-  TooLong: "client.user.agent.too.long",
+  Invalid: "client.user.agent.invalid",
 };
+
+// ASCII printable characters
+const CHARS_WHITELIST = /^[\x20-\x7E]{1,256}$/;
 
 // Stryker disable all
 export const ClientUserAgent = z
   // Stryker restore all
   .string(ClientUserAgentError.Type)
-  .min(1, ClientUserAgentError.Empty)
-  .max(128, ClientUserAgentError.TooLong)
+  .regex(CHARS_WHITELIST, ClientUserAgentError.Invalid)
   .brand("ClientUserAgent");
 
 export type ClientUserAgentType = z.infer<typeof ClientUserAgent>;
