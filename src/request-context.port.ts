4fcf3b08f2c8928c70603798ce127b1a32da52c9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-22 14:50:47 +0100

    step: add capabilities

--- src/request-context.port.ts
+++ src/request-context.port.ts
@@ -15,3 +15,35 @@ export interface RequestContext {
     ua(): string | undefined;
   };
 }
+
+export interface HasRequestPath {
+  readonly request: { readonly path: string };
+}
+
+export interface HasRequestHeader {
+  readonly request: { header(name: string): string | undefined };
+}
+
+export interface HasRequestQuery {
+  readonly request: { query(): Record<string, string> };
+}
+
+export interface HasRequestCookie {
+  readonly request: { cookie(name: string): string | undefined };
+}
+
+export interface HasRequestJSON {
+  readonly request: { json(): Promise<Record<string, unknown>> };
+}
+
+export interface HasIdentityUserId {
+  readonly identity: { userId(): UUIDType | undefined };
+}
+
+export interface HasIdentityIp {
+  readonly identity: { ip(): string | undefined };
+}
+
+export interface HasIdentityU {
+  readonly identity: { ua(): string | undefined };
+}
