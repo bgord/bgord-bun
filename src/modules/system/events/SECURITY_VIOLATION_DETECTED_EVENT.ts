df6c90a057fef5d51352d5cd3f90db6dced1614c
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-22 11:55:05 +0100

    step: make client ip and ua optional in event

--- src/modules/system/events/SECURITY_VIOLATION_DETECTED_EVENT.ts
+++ src/modules/system/events/SECURITY_VIOLATION_DETECTED_EVENT.ts
@@ -13,7 +13,7 @@ export const SecurityViolationDetectedEvent = z.object({
   name: z.literal(SECURITY_VIOLATION_DETECTED_EVENT),
   payload: z.object({
     rule: SecurityRuleName,
-    client: z.object({ ip: ClientIp, ua: ClientUserAgent }),
+    client: z.object({ ip: ClientIp.optional(), ua: ClientUserAgent.optional() }),
     userId: UUID.or(z.undefined()),
     countermeasure: SecurityCountermeasureName,
     action: z.string(),
