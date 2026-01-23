7d2b21f55ade4c5b00c55136270854341bb3f897
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-23 11:41:13 +0100

    step: add is directory to port

--- src/file-inspection.port.ts
+++ src/file-inspection.port.ts
@@ -3,6 +3,8 @@ import type * as tools from "@bgord/tools";
 export interface FileInspectionPort {
   exists(path: tools.FilePathRelative | tools.FilePathAbsolute | string): Promise<boolean>;
 
+  isDirectory(path: tools.DirectoryPathAbsoluteType | tools.DirectoryPathRelativeType): Promise<boolean>;
+
   canRead(path: tools.FilePathRelative | tools.FilePathAbsolute | string): Promise<boolean>;
   canWrite(path: tools.FilePathRelative | tools.FilePathAbsolute | string): Promise<boolean>;
   canExecute(path: tools.FilePathRelative | tools.FilePathAbsolute | string): Promise<boolean>;
