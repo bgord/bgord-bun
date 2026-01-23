f5b3eee3c20efe485ddfbbc7121f1778656a136f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-23 16:30:28 +0100

    refactor: use file reader raw in gzip adapter

--- src/gzip.adapter.ts
+++ src/gzip.adapter.ts
@@ -1,9 +1,14 @@
 import type * as tools from "@bgord/tools";
+import type { FileReaderRawPort } from "./file-reader-raw.port";
 import type { GzipPort, GzipRecipe } from "./gzip.port";
 
+type Dependencies = { FileReaderRaw: FileReaderRawPort };
+
 export class GzipAdapter implements GzipPort {
+  constructor(private readonly deps: Dependencies) {}
+
   async pack(recipe: GzipRecipe): Promise<tools.FilePathRelative | tools.FilePathAbsolute> {
-    const file = await Bun.file(recipe.input.get()).arrayBuffer();
+    const file = await this.deps.FileReaderRaw.read(recipe.input);
     const archive = Bun.gzipSync(file);
 
     await Bun.write(recipe.output.get(), archive);
--- tests/gzip.adapter.test.ts
+++ tests/gzip.adapter.test.ts
@@ -1,35 +1,35 @@
 import { describe, expect, spyOn, test } from "bun:test";
 import * as tools from "@bgord/tools";
+import { FileReaderRawNoopAdapter } from "../src/file-reader-raw-noop.adapter";
 import { GzipAdapter } from "../src/gzip.adapter";
 import * as mocks from "./mocks";
 
 const input = tools.FilePathAbsolute.fromString("/var/uploads/sample.txt");
 const output = tools.FilePathAbsolute.fromString("/var/uploads/sample.txt.gz");
-const file = new TextEncoder().encode("hello world").buffer;
+
+const content = new TextEncoder().encode("hello world").buffer;
 const gzipped = new Uint8Array([31, 139, 8, 0, 0, 0]);
 
-const adapter = new GzipAdapter();
+const FileReaderRaw = new FileReaderRawNoopAdapter(content);
+const deps = { FileReaderRaw };
+
+const adapter = new GzipAdapter(deps);
 
 describe("GzipAdapter", () => {
   test("absolute to absolute", async () => {
-    // @ts-expect-error TODO
-    const bunFile = spyOn(Bun, "file").mockReturnValue({ arrayBuffer: async () => file });
     const bunGzipSync = spyOn(Bun, "gzipSync").mockReturnValue(gzipped);
     const bunWrite = spyOn(Bun, "write").mockResolvedValue(0);
 
     const result = await adapter.pack({ input, output });
 
     expect(result).toEqual(output);
-    expect(bunFile).toHaveBeenCalledWith(input.get());
-    expect(bunGzipSync).toHaveBeenCalledWith(file);
+    expect(bunGzipSync).toHaveBeenCalledWith(content);
     expect(bunWrite).toHaveBeenCalledWith(output.get(), gzipped);
   });
 
   test("relative to relative", async () => {
     const input = tools.FilePathRelative.fromString("fixtures/sample.txt");
     const output = tools.FilePathRelative.fromString("fixtures/sample.txt.gz");
-    // @ts-expect-error TODO
-    spyOn(Bun, "file").mockReturnValue({ arrayBuffer: async () => file });
     spyOn(Bun, "gzipSync").mockReturnValue(gzipped);
     const bunWrite = spyOn(Bun, "write").mockResolvedValue(0);
 
@@ -40,8 +40,7 @@ describe("GzipAdapter", () => {
   });
 
   test("read error propagation", async () => {
-    // @ts-expect-error TODO
-    spyOn(Bun, "file").mockReturnValue({ arrayBuffer: mocks.throwIntentionalErrorAsync });
+    spyOn(FileReaderRaw, "read").mockImplementation(mocks.throwIntentionalErrorAsync);
     const bunGzipSync = spyOn(Bun, "gzipSync");
     const bunWrite = spyOn(Bun, "write");
 
@@ -51,8 +50,6 @@ describe("GzipAdapter", () => {
   });
 
   test("write error propagation", async () => {
-    // @ts-expect-error TODO
-    spyOn(Bun, "file").mockReturnValue({ arrayBuffer: async () => file });
     spyOn(Bun, "gzipSync").mockReturnValue(gzipped);
     spyOn(Bun, "write").mockRejectedValue(mocks.IntentionalError);
 
