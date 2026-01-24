03a6f50d85df3bee6ab7f0835f473c9a200e70f6
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-24 12:14:53 +0100

    refactor: security countermeasure tarpit

--- tests/security-countermeasure-tarpit.strategy.test.ts
+++ tests/security-countermeasure-tarpit.strategy.test.ts
@@ -1,41 +1,41 @@
-import { describe, expect, spyOn, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 import * as tools from "@bgord/tools";
 import { CorrelationStorage } from "../src/correlation-storage.service";
-import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
 import { SecurityContext } from "../src/security-context.vo";
 import { SecurityCountermeasureName } from "../src/security-countermeasure-name.vo";
 import { SecurityCountermeasureTarpitStrategy } from "../src/security-countermeasure-tarpit.strategy";
 import { SecurityRulePassStrategy } from "../src/security-rule-pass.strategy";
 import * as mocks from "./mocks";
 
-const Logger = new LoggerNoopAdapter();
-const deps = { Logger };
+const rule = new SecurityRulePassStrategy();
 
 const config = { duration: tools.Duration.Seconds(5), after: { kind: "allow" } as const };
-const countermeasure = new SecurityCountermeasureTarpitStrategy(deps, config);
-const rule = new SecurityRulePassStrategy();
-const context = new SecurityContext(rule.name, countermeasure.name, mocks.client, undefined);
 
 describe("SecurityCountermeasureTarpitStrategy", () => {
   test("happy path", async () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const countermeasure = new SecurityCountermeasureTarpitStrategy({ Logger }, config);
+    const context = new SecurityContext(rule.name, countermeasure.name, mocks.client, undefined);
 
     await CorrelationStorage.run(mocks.correlationId, async () => {
-      const action = await countermeasure.execute(context);
-
-      expect(action).toEqual({ kind: "delay", ...config });
+      expect(await countermeasure.execute(context)).toEqual({ kind: "delay", ...config });
     });
-
-    expect(loggerInfo).toHaveBeenCalledWith({
-      message: "Security countermeasure tarpit",
-      component: "security",
-      operation: "security_countermeasure_tarpit",
-      correlationId: mocks.correlationId,
-      metadata: context,
-    });
+    expect(Logger.entries).toEqual([
+      {
+        message: "Security countermeasure tarpit",
+        component: "security",
+        operation: "security_countermeasure_tarpit",
+        correlationId: mocks.correlationId,
+        metadata: context,
+      },
+    ]);
   });
 
   test("name", () => {
+    const Logger = new LoggerCollectingAdapter();
+    const countermeasure = new SecurityCountermeasureTarpitStrategy({ Logger }, config);
+
     expect(countermeasure.name).toEqual(SecurityCountermeasureName.parse("tarpit"));
   });
 });
