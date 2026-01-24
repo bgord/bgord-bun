baa64fb5be71bd838fbd2a123e368fcc7dbe0d8b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-24 10:28:49 +0100

    refactor: use collecting logger in event handler with logger

--- tests/event-handler-with-logger.strategy.test.ts
+++ tests/event-handler-with-logger.strategy.test.ts
@@ -1,41 +1,39 @@
-import { describe, expect, spyOn, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 import * as tools from "@bgord/tools";
 import { ClockFixedAdapter } from "../src/clock-fixed.adapter";
 import { EventHandlerWithLoggerStrategy } from "../src/event-handler-with-logger.strategy";
-import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
 import * as mocks from "./mocks";
 
 const event = { name: "user.created" };
 
-const Logger = new LoggerNoopAdapter();
 const Clock = new ClockFixedAdapter(mocks.TIME_ZERO);
-const deps = { Logger, Clock };
-
-const handler = new EventHandlerWithLoggerStrategy(deps);
 
 describe("EventHandlerWithLoggerStrategy", () => {
   test("happy path", async () => {
-    const loggerErrorSpy = spyOn(Logger, "error");
+    const Logger = new LoggerCollectingAdapter();
+    const handler = new EventHandlerWithLoggerStrategy({ Clock, Logger });
     const fn = async (_event: typeof event) => {};
 
     await handler.handle(fn)(event);
 
-    expect(loggerErrorSpy).not.toHaveBeenCalled();
+    expect(Logger.entries.length).toEqual(0);
   });
 
   test("error path", async () => {
-    const loggerError = spyOn(Logger, "error");
+    const Logger = new LoggerCollectingAdapter();
+    const handler = new EventHandlerWithLoggerStrategy({ Clock, Logger });
 
     await handler.handle(mocks.throwIntentionalErrorAsync)(event);
 
-    expect(loggerError).toHaveBeenCalledTimes(1);
-
-    expect(loggerError).toHaveBeenCalledWith({
-      message: "Unknown user.created event handler error",
-      component: "infra",
-      operation: "event_handler",
-      metadata: { ...event, duration: expect.any(tools.Duration) },
-      error: new Error(mocks.IntentionalError),
-    });
+    expect(Logger.entries).toEqual([
+      {
+        message: "Unknown user.created event handler error",
+        component: "infra",
+        operation: "event_handler",
+        metadata: { ...event, duration: expect.any(tools.Duration) },
+        error: new Error(mocks.IntentionalError),
+      },
+    ]);
   });
 });
