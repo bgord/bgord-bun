d3df574797a704d79ed18dc7b14abe6d96077eaf
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-24 11:23:03 +0100

    refactor: use collecting logger in preqreq startup runner

--- tests/prerequisite-runner-startup.service.test.ts
+++ tests/prerequisite-runner-startup.service.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, spyOn, test } from "bun:test";
 import { ClockFixedAdapter } from "../src/clock-fixed.adapter";
-import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
 import { Prerequisite } from "../src/prerequisite.vo";
 import { PrerequisiteRunnerStartup } from "../src/prerequisite-runner-startup.service";
 import {
@@ -10,66 +10,62 @@ import {
 } from "../src/prerequisite-verifier.port";
 import * as mocks from "./mocks";
 
-const Logger = new LoggerNoopAdapter();
 const Clock = new ClockFixedAdapter(mocks.TIME_ZERO);
-const deps = { Logger, Clock };
-
-const runner = new PrerequisiteRunnerStartup(deps);
 
 describe("PrerequisiteRunnerStartup service", () => {
   test("happy path", async () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const runner = new PrerequisiteRunnerStartup({ Logger, Clock });
 
     await runner.check([mocks.PrerequisiteOk, mocks.PrerequisiteOk]);
 
-    expect(loggerInfo).toHaveBeenCalledWith({
-      component: "infra",
-      operation: "startup",
-      message: "Prerequisites ok",
-    });
+    expect(Logger.entries).toEqual([
+      { component: "infra", operation: "startup", message: "Prerequisites ok" },
+    ]);
   });
 
   test("failure", async () => {
-    const loggerError = spyOn(Logger, "error");
+    const Logger = new LoggerCollectingAdapter();
+    const runner = new PrerequisiteRunnerStartup({ Logger, Clock });
 
     expect(async () => runner.check([mocks.PrerequisiteOk, mocks.PrerequisiteFail])).toThrow(
       "prerequisites.failure",
     );
-    expect(loggerError).toHaveBeenCalledWith({
-      component: "infra",
-      operation: "startup",
-      message: "Prerequisite failed",
-      metadata: { label: "fail", kind: "test" },
-      error: { message: mocks.IntentionalError },
-    });
+    expect(Logger.entries).toEqual([
+      {
+        component: "infra",
+        operation: "startup",
+        message: "Prerequisite failed",
+        metadata: { label: "fail", kind: "test" },
+        error: { message: mocks.IntentionalError },
+      },
+    ]);
   });
 
   test("undetermined", async () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const runner = new PrerequisiteRunnerStartup({ Logger, Clock });
 
     await runner.check([mocks.PrerequisiteOk, mocks.PrerequisiteUndetermined]);
 
-    expect(loggerInfo).toHaveBeenCalledWith({
-      component: "infra",
-      operation: "startup",
-      message: "Prerequisites ok",
-    });
+    expect(Logger.entries).toEqual([
+      { component: "infra", operation: "startup", message: "Prerequisites ok" },
+    ]);
   });
 
   test("skips disabled", async () => {
+    const Logger = new LoggerCollectingAdapter();
+    const runner = new PrerequisiteRunnerStartup({ Logger, Clock });
     const PrerequisiteDisabled = new Prerequisite("undetermined", new mocks.PrerequisiteVerifierPass(), {
       enabled: false,
     });
-    const loggerInfo = spyOn(Logger, "info");
     const prerequisiteDisabledBuild = spyOn(PrerequisiteDisabled, "build");
 
     await runner.check([mocks.PrerequisiteOk, PrerequisiteDisabled]);
 
-    expect(loggerInfo).toHaveBeenCalledWith({
-      component: "infra",
-      operation: "startup",
-      message: "Prerequisites ok",
-    });
+    expect(Logger.entries).toEqual([
+      { component: "infra", operation: "startup", message: "Prerequisites ok" },
+    ]);
     expect(prerequisiteDisabledBuild).not.toHaveBeenCalled();
   });
 
@@ -84,18 +80,21 @@ describe("PrerequisiteRunnerStartup service", () => {
       }
     }
     const PrerequisiteError = new Prerequisite("error", new PrerequisiteVerifierError());
-    const loggerError = spyOn(Logger, "error");
+    const Logger = new LoggerCollectingAdapter();
+    const runner = new PrerequisiteRunnerStartup({ Logger, Clock });
 
     expect(async () => runner.check([PrerequisiteError])).toThrow("prerequisites.failure");
-    expect(loggerError).toHaveBeenCalledWith({
-      component: "infra",
-      error: new Error("Unknown error"),
-      message: "Prerequisite failed",
-      metadata: {
-        kind: PrerequisiteError.kind,
-        label: "error",
+    expect(Logger.entries).toEqual([
+      {
+        component: "infra",
+        error: new Error("Unknown error"),
+        message: "Prerequisite failed",
+        metadata: {
+          kind: PrerequisiteError.kind,
+          label: "error",
+        },
+        operation: "startup",
       },
-      operation: "startup",
-    });
+    ]);
   });
 });
