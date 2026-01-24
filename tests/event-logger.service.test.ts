01410c4e0f4dd242bb8b6e92e7160f51851df261
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-24 10:30:21 +0100

    refactor: use collecting logger in event logger

--- tests/event-logger.service.test.ts
+++ tests/event-logger.service.test.ts
@@ -1,42 +1,42 @@
-import { describe, expect, spyOn, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 import { EventLogger } from "../src/event-logger.service";
-import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
 
 const eventName = "user.created";
 
-const Logger = new LoggerNoopAdapter();
-const deps = { Logger };
-
-const eventLogger = new EventLogger(deps);
-
 describe("EventLogger service", () => {
   test("happy path", () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const eventLogger = new EventLogger({ Logger });
 
     eventLogger.handle("emit", "debug:name", eventName, { userId: 123 });
 
-    expect(loggerInfo).toHaveBeenCalledWith({
-      message: `${eventName} emitted`,
-      component: "infra",
-      operation: "event_emitted",
-      metadata: { userId: 123 },
-    });
+    expect(Logger.entries).toEqual([
+      {
+        message: `${eventName} emitted`,
+        component: "infra",
+        operation: "event_emitted",
+        metadata: { userId: 123 },
+      },
+    ]);
   });
 
   test("does not log subscribe commands", () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const eventLogger = new EventLogger({ Logger });
 
     eventLogger.handle("subscribe", "debug:name", eventName, { userId: 123 });
 
-    expect(loggerInfo).not.toHaveBeenCalled();
+    expect(Logger.entries.length).toEqual(0);
   });
 
   test("does not log commands with symbol names", () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const eventLogger = new EventLogger({ Logger });
 
     // @ts-expect-error Symbol usage
     eventLogger.handle("emit", "debug:name", Symbol(eventName), { userId: 123 });
 
-    expect(loggerInfo).not.toHaveBeenCalled();
+    expect(Logger.entries.length).toEqual(0);
   });
 });
