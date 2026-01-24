f6e2571c74e1e82c5616757e559d7a968e2bd0f0
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-24 12:07:16 +0100

    refactor: pdf generator noop

--- tests/pdf-generator-noop.adapter.test.ts
+++ tests/pdf-generator-noop.adapter.test.ts
@@ -1,25 +1,23 @@
-import { describe, expect, spyOn, test } from "bun:test";
-import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
+import { describe, expect, test } from "bun:test";
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
 import { PdfGeneratorNoopAdapter } from "../src/pdf-generator-noop.adapter";
 
 const PLACEHOLDER_PDF_BASE64 =
   "JVBERi0xLjQKMSAwIG9iago8PC9UeXBlIC9DYXRhbG9nCi9QYWdlcyAyIDAgUgo+PgplbmRvYmoK MiAwIG9iago8PC9UeXBlIC9QYWdlcwovS2lkcyBbMyAwIFJdCi9Db3VudCAxCj4+CmVuZG9iagoz IDAgb2JqCjw8L1R5cGUgL1BhZ2UKL1BhcmVudCAyIDAgUgovTWVkaWFCb3ggWzAgMCA1OTUgODQy XQovQ29udGVudHMgNSAwIFIKL1Jlc291cmNlcyA8PC9Qcm9jU2V0IFsvUERGIC9UZXh0XQovRm9u dCA8PC9GMSA0IDAgUj4+Cj4+Cj4+CmVuZG9iago0IDAgb2JqCjw8L1R5cGUgL0ZvbnQKL1N1YnR5 cGUgL1R5cGUxCi9OYW1lIC9GMQovQmFzZUZvbnQgL0hlbHZldGljYQovRW5jb2RpbmcgL01hY1Jv bWFuRW5jb2RpbmcKPj4KZW5kb2JqCjUgMCBvYmoKPDwvTGVuZ3RoIDUzCj4+CnN0cmVhbQpCVAov RjEgMjAgVGYKMjIwIDQwMCBUZAooRHVtbXkgUERGKSBUagpFVAplbmRzdHJlYW0KZW5kb2JqCnhy ZWYKMCA2CjAwMDAwMDAwMDAgNjU1MzUgZgowMDAwMDAwMDA5IDAwMDAwIG4KMDAwMDAwMDA2MyAw MDAwMCBuCjAwMDAwMDAxMjQgMDAwMDAgbgowMDAwMDAwMjc3IDAwMDAwIG4KMDAwMDAwMDM5MiAw MDAwMCBuCnRyYWlsZXIKPDwvU2l6ZSA2Ci9Sb290IDEgMCBSCj4+CnN0YXJ0eHJlZgo0OTUKJSVF T0YK";
 
-const Logger = new LoggerNoopAdapter();
-const deps = { Logger };
-
-const adapter = new PdfGeneratorNoopAdapter(deps);
-
 describe("PdfGeneratorNoopAdapter", () => {
   test("success", async () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const adapter = new PdfGeneratorNoopAdapter({ Logger });
 
     expect(await adapter.request("welcome", {})).toEqual(Buffer.from(PLACEHOLDER_PDF_BASE64, "base64"));
-    expect(loggerInfo).toHaveBeenCalledWith({
-      component: "infra",
-      operation: "pdf_generator",
-      metadata: { data: {}, template: "welcome" },
-      message: "[NOOP] PDF generator adapter",
-    });
+    expect(Logger.entries).toEqual([
+      {
+        component: "infra",
+        operation: "pdf_generator",
+        metadata: { data: {}, template: "welcome" },
+        message: "[NOOP] PDF generator adapter",
+      },
+    ]);
   });
 });
