b72f08cf26e32c429dcaac4b5a5b0b12a3c07fd7
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-24 10:26:58 +0100

    refactor: use collecting logger in command logger

--- tests/command-logger.service.test.ts
+++ tests/command-logger.service.test.ts
@@ -1,40 +1,40 @@
-import { describe, expect, spyOn, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 import { CommandLogger } from "../src/command-logger.service";
-import { LoggerNoopAdapter } from "../src/logger-noop.adapter";
-
-const Logger = new LoggerNoopAdapter();
-const deps = { Logger };
-
-const commandLogger = new CommandLogger(deps);
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
 
 describe("CommandLogger service", () => {
   test("logs emitted command with metadata", () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const commandLogger = new CommandLogger({ Logger });
 
     commandLogger.handle("emit", "debug:name", "user.created", { userId: 123 });
 
-    expect(loggerInfo).toHaveBeenCalledWith({
-      message: "user.created emitted",
-      component: "infra",
-      operation: "command_emitted",
-      metadata: { userId: 123 },
-    });
+    expect(Logger.entries).toEqual([
+      {
+        message: "user.created emitted",
+        component: "infra",
+        operation: "command_emitted",
+        metadata: { userId: 123 },
+      },
+    ]);
   });
 
   test("does not log subscribe commands", () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const commandLogger = new CommandLogger({ Logger });
 
     commandLogger.handle("subscribe", "debug:name", "user.created", { userId: 123 });
 
-    expect(loggerInfo).not.toHaveBeenCalled();
+    expect(Logger.entries.length).toEqual(0);
   });
 
   test("does not log commands with symbol names", () => {
-    const loggerInfo = spyOn(Logger, "info");
+    const Logger = new LoggerCollectingAdapter();
+    const commandLogger = new CommandLogger({ Logger });
 
     // @ts-expect-error Symbol usage
     commandLogger.handle("emit", "debug:name", Symbol("user.created"), { userId: 123 });
 
-    expect(loggerInfo).not.toHaveBeenCalled();
+    expect(Logger.entries.length).toEqual(0);
   });
 });
