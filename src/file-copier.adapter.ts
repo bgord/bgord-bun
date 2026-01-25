cb66273b1029b5efdb02a342b777e877a3ecf549
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-25 16:05:38 +0100

    fix: mutants in file file copier

--- src/file-copier.adapter.ts
+++ src/file-copier.adapter.ts
@@ -6,7 +6,9 @@ export class FileCopierAdapter implements FileCopierPort {
     source: tools.FilePathRelative | tools.FilePathAbsolute | string,
     destination: tools.FilePathRelative | tools.FilePathAbsolute | string,
   ): Promise<void> {
+    // Stryker disable all
     const file = Bun.file(typeof source === "string" ? source : source.get());
+    // Stryker restore all
 
     await Bun.write(typeof destination === "string" ? destination : destination.get(), file);
   }
