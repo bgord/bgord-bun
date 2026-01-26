56d90af060e637bd4984d0acdf05a8be3b2e88fb
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-26 15:12:23 +0100

    step: add sampling every nth

--- src/index.ts
+++ src/index.ts
@@ -340,6 +340,7 @@ export * from "./woodchopper-dispatcher-noop.strategy";
 export * from "./woodchopper-dispatcher-sampling.strategy";
 export * from "./woodchopper-dispatcher-sync.strategy";
 export * from "./woodchopper-sampling.strategy";
+export * from "./woodchopper-sampling-every-nth.strategy";
 export * from "./woodchopper-sink.strategy";
 export * from "./woodchopper-sink-collecting.strategy";
 export * from "./woodchopper-sink-noop.strategy";
---
+++ src/woodchopper-sampling-every-nth.strategy.ts
@@ -0,0 +1,19 @@
+import type * as tools from "@bgord/tools";
+import type { LoggerEntry } from "./logger.port";
+import { LogLevelEnum } from "./logger.port";
+import type { WoodchoperSamplingStrategy } from "./woodchopper-sampling.strategy";
+
+export class WoodchopperSamplingEveryNth implements WoodchoperSamplingStrategy {
+  constructor(private readonly n: tools.IntegerPositiveType) {}
+
+  private counter = 0;
+
+  decide(entry: LoggerEntry): boolean {
+    if ([LogLevelEnum.error, LogLevelEnum.warn].includes(entry.level)) return true;
+
+    this.counter++;
+
+    if (this.counter % this.n !== 0) return false;
+    return true;
+  }
+}
