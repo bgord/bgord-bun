7b5ff6d63c14b91dd8042a95c56cd5b63b304fc9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-01 09:41:12 +0100

    fix: better auth types in auth session reader adapter

--- src/auth-session-reader-better-auth.adapter.ts
+++ src/auth-session-reader-better-auth.adapter.ts
@@ -2,18 +2,18 @@ import type { betterAuth } from "better-auth";
 import type { AuthSessionReaderPort } from "./auth-session-reader.port";
 import type { HasRequestHeaders } from "./request-context.port";
 
-type BetterAuthUserType = ReturnType<typeof betterAuth>["$Infer"]["Session"]["user"];
-type BetterAuthSessionType = ReturnType<typeof betterAuth>["$Infer"]["Session"]["session"];
+type BetterAuthInstance = ReturnType<typeof betterAuth>;
 
-export class AuthSessionReaderBetterAuthAdapter
-  implements AuthSessionReaderPort<BetterAuthUserType, BetterAuthSessionType>
+export class AuthSessionReaderBetterAuthAdapter<TAuth extends BetterAuthInstance>
+  implements AuthSessionReaderPort<TAuth["$Infer"]["Session"]["user"], TAuth["$Infer"]["Session"]["session"]>
 {
-  constructor(private readonly auth: ReturnType<typeof betterAuth>) {}
+  constructor(private readonly auth: TAuth) {}
 
   async getSession(context: HasRequestHeaders) {
     const result = await this.auth.api.getSession({ headers: context.request.headers() });
 
     if (!result) return { user: null, session: null };
+
     return { user: result.user, session: result.session };
   }
 }
