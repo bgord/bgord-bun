adb6869b7d1f1dfa9a8d35b7ceb39efec344c4df
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-02 22:32:31 +0100

    refactor: update ab hono noop middleware

--- src/ab-hono-noop.middleware.ts
+++ src/ab-hono-noop.middleware.ts
@@ -1,13 +1,24 @@
 import type { MiddlewareHandler } from "hono";
+import { AbMiddleware } from "./ab.middleware";
+import { AbAssignmentFixedStrategy } from "./ab-assignment-fixed.strategy";
 import type { AbVariant } from "./ab-variant.vo";
+import type { AbVariants } from "./ab-variants.vo";
 import type { MiddlewareHonoPort } from "./middleware-hono.port";
+import { RequestContextAdapterHono } from "./request-context-hono.adapter";
 
 export class AbHonoNoopMiddleware implements MiddlewareHonoPort {
-  constructor(private readonly variant: AbVariant) {}
+  private readonly middleware: AbMiddleware;
+
+  constructor(variants: AbVariants, variant: AbVariant) {
+    this.middleware = new AbMiddleware(variants, new AbAssignmentFixedStrategy(variant));
+  }
 
   handle(): MiddlewareHandler {
     return async (c, next) => {
-      c.set("abVariant", this.variant);
+      const context = new RequestContextAdapterHono(c);
+      const variant = await this.middleware.evaluate(context);
+
+      c.set("abVariant", variant);
 
       return next();
     };
--- tests/ab-hono-noop.middleware.test.ts
+++ tests/ab-hono-noop.middleware.test.ts
@@ -4,19 +4,22 @@ import type { AbVariables } from "../src/ab-hono.middleware";
 import { AbHonoNoopMiddleware } from "../src/ab-hono-noop.middleware";
 import { AbVariant } from "../src/ab-variant.vo";
 import { AbVariantWeight } from "../src/ab-variant-weight.vo";
+import { AbVariants } from "../src/ab-variants.vo";
 
 const control = new AbVariant({ name: "control", weight: AbVariantWeight.parse(50) });
-const ab = new AbHonoNoopMiddleware(control).handle();
+const treatment = new AbVariant({ name: "treatment", weight: AbVariantWeight.parse(50) });
+const variants = new AbVariants([control, treatment]);
+
+const ab = new AbHonoNoopMiddleware(variants, control).handle();
 
 describe("AbHonoNoopMiddleware", () => {
   test("happy path", async () => {
     const app = new Hono<{ Variables: AbVariables }>()
       .use(ab)
-      .get("/test", (c) => c.json({ variant: c.get("abVariant").config.name }));
+      .get("/test", (c) => c.text(c.get("abVariant").config.name));
 
     const response = await app.request("/test");
-    const json = await response.json();
 
-    expect(json.variant).toEqual("control");
+    expect(await response.text()).toEqual("control");
   });
 });
