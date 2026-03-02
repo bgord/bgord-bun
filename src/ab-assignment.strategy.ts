cd24ac889799be2bbd75d4179076d50b9e65529e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-02 22:18:57 +0100

    step: define ab assignment strategy

---
+++ src/ab-assignment.strategy.ts
@@ -0,0 +1,7 @@
+import type { AbVariant } from "./ab-variant.vo";
+import type { AbVariants } from "./ab-variants.vo";
+import type { RequestContext } from "./request-context.port";
+
+export interface AbAssignmentStrategy {
+  assign(context: RequestContext, variants: AbVariants): Promise<AbVariant | undefined>;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./ab.middleware";
+export * from "./ab-assignment.strategy";
 export * from "./ab-variant.vo";
 export * from "./ab-variant-selector.service";
 export * from "./ab-variant-weight.vo";
