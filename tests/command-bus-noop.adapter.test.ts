41a190ce4902cb73390de5e33d626098cadbd32b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-06 11:31:47 +0100

    refactor: unify command bus tests

--- tests/command-bus-noop.adapter.test.ts
+++ tests/command-bus-noop.adapter.test.ts
@@ -2,22 +2,14 @@ import { describe, expect, jest, test } from "bun:test";
 import { CommandBusNoopAdapter } from "../src/command-bus-noop.adapter";
 
 type CommandType = { name: "TEST_COMMAND" };
-
 const command = { name: "TEST_COMMAND" } as const;
 
 describe("CommandBusNoopAdapter", () => {
-  test("emit", async () => {
-    const bus = new CommandBusNoopAdapter<CommandType>();
-
-    expect(async () => bus.emit("TEST_COMMAND", command)).not.toThrow();
-  });
-
-  test("on", async () => {
+  test("happy path", async () => {
     const handler = jest.fn();
     const bus = new CommandBusNoopAdapter<CommandType>();
 
     bus.on("TEST_COMMAND", handler);
-
     await bus.emit("TEST_COMMAND", command);
 
     expect(handler).not.toHaveBeenCalled();
