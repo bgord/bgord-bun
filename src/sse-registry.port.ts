157184ad40169d5788fa368a9fabf79d9e8b35fb
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-10 12:00:33 +0100

    step: add sse registry port

---
+++ src/sse-registry.port.ts
@@ -0,0 +1,10 @@
+import type { Message } from "./message.types";
+import type { SseConnectionPort } from "./sse-connection.port";
+
+export interface SseRegistryPort<Messages extends Message> {
+  register(userId: string, connection: SseConnectionPort): void;
+
+  unregister(userId: string, connection: SseConnectionPort): void;
+
+  emit<M extends Messages>(userId: string, message: M): Promise<void>;
+}
