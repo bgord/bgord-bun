22ac3180821edd4b35b2f1f36e0b1d6b3bddea4a
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-10 15:01:20 +0100

    refactor: remove on close from sse connection port

--- src/sse-connection.port.ts
+++ src/sse-connection.port.ts
@@ -3,7 +3,5 @@ import type { Message } from "./message.types";
 export interface SseConnectionPort<Messages extends Message> {
   send<M extends Messages>(message: M): Promise<void>;
 
-  close(): void;
-
-  onClose(callback: () => void): void;
+  close(callback: () => void): void;
 }
