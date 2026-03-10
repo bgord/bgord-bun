f8f28d0f8416511197f82d351393a02187d97339
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-10 14:03:44 +0100

    step: add sse connection with logger adapter

---
+++ src/sse-connection-with-logger.adapter.ts
@@ -0,0 +1,47 @@
+import { CorrelationStorage } from "./correlation-storage.service";
+import type { LoggerPort } from "./logger.port";
+import type { Message } from "./message.types";
+import type { SseConnectionPort } from "./sse-connection.port";
+
+type Dependencies<Messages extends Message> = { inner: SseConnectionPort<Messages>; Logger: LoggerPort };
+
+export class SseConnectionWithLoggerAdapter<Messages extends Message> implements SseConnectionPort<Messages> {
+  private readonly base = { component: "infra", operation: "sse_connection" };
+
+  constructor(private readonly deps: Dependencies<Messages>) {}
+
+  async send<M extends Messages>(message: M) {
+    this.deps.Logger.debug({
+      message: `${message.name} sent`,
+      metadata: message,
+      correlationId: CorrelationStorage.get(),
+      ...this.base,
+    });
+
+    await this.deps.inner.send(message);
+  }
+
+  close(): void {
+    this.deps.Logger.debug({
+      message: "sse_connection_closed",
+      metadata: {},
+      correlationId: CorrelationStorage.get(),
+      ...this.base,
+    });
+
+    this.deps.inner.close();
+  }
+
+  onClose(callback: () => void): void {
+    this.deps.inner.onClose(() => {
+      this.deps.Logger.debug({
+        message: "sse_connection_client_disconnected",
+        metadata: {},
+        correlationId: CorrelationStorage.get(),
+        ...this.base,
+      });
+
+      callback();
+    });
+  }
+}
