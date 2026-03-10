c70233067271205df9259e3525b1bce08776fcd0
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-10 12:24:19 +0100

    refactor: make sse registry collecting type safe

--- src/sse-registry-collecting.adapter.ts
+++ src/sse-registry-collecting.adapter.ts
@@ -5,8 +5,9 @@ import type { SseRegistryPort } from "./sse-registry.port";
 export class SseRegistryCollectingAdapter<Messages extends Message> implements SseRegistryPort<Messages> {
   public emitted: Array<{ userId: string; message: Messages }> = [];
 
-  register(_userId: string, _connection: SseConnectionPort): void {}
-  unregister(_userId: string, _connection: SseConnectionPort): void {}
+  register(_userId: string, _connection: SseConnectionPort<Messages>): void {}
+
+  unregister(_userId: string, _connection: SseConnectionPort<Messages>): void {}
 
   async emit<M extends Messages>(userId: string, message: M): Promise<void> {
     this.emitted.push({ userId, message });
