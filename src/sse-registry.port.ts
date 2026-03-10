05a857b68defda2d5c00b6c307c24af6c371161e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-10 12:17:09 +0100

    step: add sse connection noop adapter

--- src/sse-registry.port.ts
+++ src/sse-registry.port.ts
@@ -2,9 +2,9 @@ import type { Message } from "./message.types";
 import type { SseConnectionPort } from "./sse-connection.port";
 
 export interface SseRegistryPort<Messages extends Message> {
-  register(userId: string, connection: SseConnectionPort): void;
+  register(userId: string, connection: SseConnectionPort<Messages>): void;
 
-  unregister(userId: string, connection: SseConnectionPort): void;
+  unregister(userId: string, connection: SseConnectionPort<Messages>): void;
 
   emit<M extends Messages>(userId: string, message: M): Promise<void>;
 }
