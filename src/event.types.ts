fcdc596f26f5a8c5406e62dc863d77376ea6c213
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-15 09:11:53 +0100

    step: add generic event types

--- src/event.types.ts
+++ src/event.types.ts
@@ -14,3 +14,17 @@ export type GenericEventSchema = z.ZodObject<{
 export type GenericParsedEventSchema = z.ZodObject<
   Omit<GenericEventSchema["shape"], "payload"> & { payload: z.ZodString }
 >;
+
+// TODO: use proper VO types.
+export type GenericEvent = {
+  id: string;
+  correlationId: string;
+  createdAt: number;
+  stream: string;
+  revision?: number;
+  name: string;
+  version: number;
+  payload: unknown;
+};
+
+export type GenericParsedEvent = Omit<GenericEvent, "payload"> & { payload: string };
