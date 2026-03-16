36f8f7543903550a47612161e37fe04e8838a4c5
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:02:52 +0100

    refactor: rewrite client ip

--- tests/client-ip.vo.test.ts
+++ tests/client-ip.vo.test.ts
@@ -1,20 +1,21 @@
 import { describe, expect, test } from "bun:test";
+import * as v from "valibot";
 import { ClientIp } from "../src/client-ip.vo";
 
 describe("ClientIp", () => {
   test("happy path", () => {
-    expect(ClientIp.safeParse("127.0.0.1").success).toEqual(true);
+    expect(v.safeParse(ClientIp, "127.0.0.1").success).toEqual(true);
   });
 
   test("rejects empty", () => {
-    expect(() => ClientIp.parse("")).toThrow("client.ip.invalid");
+    expect(() => v.parse(ClientIp, "")).toThrow("client.ip.invalid");
   });
 
   test("rejects non-string - null", () => {
-    expect(() => ClientIp.parse(null)).toThrow("client.ip.invalid");
+    expect(() => v.parse(ClientIp, null)).toThrow("client.ip.invalid");
   });
 
   test("rejects non-string - number", () => {
-    expect(() => ClientIp.parse(123)).toThrow("client.ip.invalid");
+    expect(() => v.parse(ClientIp, 123)).toThrow("client.ip.invalid");
   });
 });
