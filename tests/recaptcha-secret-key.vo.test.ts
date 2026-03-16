85379b99fb91fb7909f275058968f420c4b37e3f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:14:31 +0100

    refactor: rewrite recaptcha secret key

--- tests/recaptcha-secret-key.vo.test.ts
+++ tests/recaptcha-secret-key.vo.test.ts
@@ -1,25 +1,26 @@
 import { describe, expect, test } from "bun:test";
+import * as v from "valibot";
 import { RecaptchaSecretKey } from "../src/recaptcha-secret-key.vo";
 
-describe("RecaptchaSiteKey", () => {
+describe("RecaptchaSecretKey", () => {
   test("happy path", () => {
-    expect(RecaptchaSecretKey.safeParse("a".repeat(40)).success).toEqual(true);
-    expect(RecaptchaSecretKey.safeParse("A".repeat(40)).success).toEqual(true);
+    expect(v.safeParse(RecaptchaSecretKey, "a".repeat(40)).success).toEqual(true);
+    expect(v.safeParse(RecaptchaSecretKey, "A".repeat(40)).success).toEqual(true);
   });
 
   test("rejects non-string - null", () => {
-    expect(() => RecaptchaSecretKey.parse(null)).toThrow("recaptcha.secret.key.type");
+    expect(() => v.parse(RecaptchaSecretKey, null)).toThrow("recaptcha.secret.key.type");
   });
 
   test("rejects non-string - number", () => {
-    expect(() => RecaptchaSecretKey.parse(123)).toThrow("recaptcha.secret.key.type");
+    expect(() => v.parse(RecaptchaSecretKey, 123)).toThrow("recaptcha.secret.key.type");
   });
 
   test("rejects empty", () => {
-    expect(() => RecaptchaSecretKey.parse("")).toThrow("recaptcha.secret.key.length");
+    expect(() => v.parse(RecaptchaSecretKey, "")).toThrow("recaptcha.secret.key.length");
   });
 
   test("rejects too long", () => {
-    expect(() => RecaptchaSecretKey.parse(`${"a".repeat(40)}a`)).toThrow("recaptcha.secret.key.length");
+    expect(() => v.parse(RecaptchaSecretKey, `${"a".repeat(40)}a`)).toThrow("recaptcha.secret.key.length");
   });
 });
