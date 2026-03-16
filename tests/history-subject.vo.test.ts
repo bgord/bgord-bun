8f959e91350ffb4d41702f846095ea6227b9c618
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:18:33 +0100

    refactor: rewrite history subject

--- tests/history-subject.vo.test.ts
+++ tests/history-subject.vo.test.ts
@@ -1,25 +1,28 @@
 import { describe, expect, test } from "bun:test";
+import * as v from "valibot";
 import * as History from "../src/modules/history";
 
 describe("HistorySubject", () => {
   test("happy path", () => {
-    expect(History.VO.HistorySubject.safeParse("a".repeat(128)).success).toEqual(true);
-    expect(History.VO.HistorySubject.safeParse("A".repeat(128)).success).toEqual(true);
+    expect(v.safeParse(History.VO.HistorySubject, "a".repeat(128)).success).toEqual(true);
+    expect(v.safeParse(History.VO.HistorySubject, "A".repeat(128)).success).toEqual(true);
   });
 
   test("rejects non-string - null", () => {
-    expect(() => History.VO.HistorySubject.parse(null)).toThrow("history.subject.type");
+    expect(() => v.parse(History.VO.HistorySubject, null)).toThrow("history.subject.type");
   });
 
   test("rejects non-string - number", () => {
-    expect(() => History.VO.HistorySubject.parse(123)).toThrow("history.subject.type");
+    expect(() => v.parse(History.VO.HistorySubject, 123)).toThrow("history.subject.type");
   });
 
   test("rejects empty", () => {
-    expect(() => History.VO.HistorySubject.parse("")).toThrow("history.subject.empty");
+    expect(() => v.parse(History.VO.HistorySubject, "")).toThrow("history.subject.empty");
   });
 
   test("rejects too long", () => {
-    expect(() => History.VO.HistorySubject.parse(`${"a".repeat(128)}a`)).toThrow("history.subject.too.long");
+    expect(() => v.parse(History.VO.HistorySubject, `${"a".repeat(128)}a`)).toThrow(
+      "history.subject.too.long",
+    );
   });
 });
