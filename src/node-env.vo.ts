818db3bb72f6b63108af12c3133ee8bf509035de
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:24:29 +0100

    refactor: remove zod from node env

--- src/node-env.vo.ts
+++ src/node-env.vo.ts
@@ -1,12 +1,6 @@
-import * as z from "zod/v4";
-
 export enum NodeEnvironmentEnum {
   local = "local",
   test = "test",
   staging = "staging",
   production = "production",
 }
-
-export const NodeEnvironmentError = { Invalid: "node.environment.invalid" } as const;
-
-export const NodeEnvironment = z.enum(NodeEnvironmentEnum, NodeEnvironmentError.Invalid);
