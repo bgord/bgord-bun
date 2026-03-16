1e9122ea522aa07cb0cd3eb2df1fc0e539cc72c6
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:02:10 +0100

    refactor: rewrite binary

--- tests/binary.vo.test.ts
+++ tests/binary.vo.test.ts
@@ -1,36 +1,37 @@
 import { describe, expect, test } from "bun:test";
+import * as v from "valibot";
 import { Binary } from "../src/binary.vo";
 
 describe("Binary", () => {
   test("happy path", () => {
-    expect(Binary.safeParse("a".repeat(64)).success).toEqual(true);
-    expect(Binary.safeParse("A".repeat(64)).success).toEqual(true);
-    expect(Binary.safeParse("a_beta").success).toEqual(true);
-    expect(Binary.safeParse("a-beta").success).toEqual(true);
-    expect(Binary.safeParse("a2").success).toEqual(true);
+    expect(v.safeParse(Binary, "a".repeat(64)).success).toEqual(true);
+    expect(v.safeParse(Binary, "A".repeat(64)).success).toEqual(true);
+    expect(v.safeParse(Binary, "a_beta").success).toEqual(true);
+    expect(v.safeParse(Binary, "a-beta").success).toEqual(true);
+    expect(v.safeParse(Binary, "a2").success).toEqual(true);
   });
 
   test("rejects prefix", () => {
-    expect(() => Binary.parse("!a")).toThrow("binary.bad.chars");
+    expect(() => v.parse(Binary, "!a")).toThrow("binary.bad.chars");
   });
 
   test("rejects non-string - null", () => {
-    expect(() => Binary.parse(null)).toThrow("binary.type");
+    expect(() => v.parse(Binary, null)).toThrow("binary.type");
   });
 
   test("rejects non-string - number", () => {
-    expect(() => Binary.parse(123)).toThrow("binary.type");
+    expect(() => v.parse(Binary, 123)).toThrow("binary.type");
   });
 
   test("rejects empty", () => {
-    expect(() => Binary.parse("")).toThrow("binary.empty");
+    expect(() => v.parse(Binary, ""));
   });
 
   test("rejects too long", () => {
-    expect(() => Binary.parse(`${"a".repeat(64)}abc`)).toThrow("binary.too.long");
+    expect(() => v.parse(Binary, `${"a".repeat(64)}abc`)).toThrow("binary.too.long");
   });
 
   test("rejects bad chars", () => {
-    expect(() => Binary.parse(`${"a".repeat(63)}!`)).toThrow("binary.bad.chars");
+    expect(() => v.parse(Binary, `${"a".repeat(63)}!`)).toThrow("binary.bad.chars");
   });
 });
