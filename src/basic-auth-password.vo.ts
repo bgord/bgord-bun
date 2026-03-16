d4dfafbf6fe26fe449f4f493c147828da0545d32
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:00:49 +0100

    refactor: rewrite basic auth password

--- src/basic-auth-password.vo.ts
+++ src/basic-auth-password.vo.ts
@@ -1,4 +1,4 @@
-import * as z from "zod/v4";
+import * as v from "valibot";
 
 export const BasicAuthPasswordError = {
   Type: "basic.auth.password.type",
@@ -6,12 +6,12 @@ export const BasicAuthPasswordError = {
   TooLong: "basic.auth.password.too.long",
 };
 
-// Stryker disable all
-export const BasicAuthPassword = z
-  // Stryker restore all
-  .string(BasicAuthPasswordError.Type)
-  .min(1, BasicAuthPasswordError.Empty)
-  .max(128, BasicAuthPasswordError.TooLong)
-  .brand("BasicAuthPassword");
+export const BasicAuthPassword = v.pipe(
+  v.string(BasicAuthPasswordError.Type),
+  v.minLength(1, BasicAuthPasswordError.Empty),
+  v.maxLength(128, BasicAuthPasswordError.TooLong),
+  // Stryker disable next-line StringLiteral
+  v.brand("BasicAuthPassword"),
+);
 
-export type BasicAuthPasswordType = z.infer<typeof BasicAuthPassword>;
+export type BasicAuthPasswordType = v.InferOutput<typeof BasicAuthPassword>;
