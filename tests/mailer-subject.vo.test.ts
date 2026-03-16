e886acc997a0e749ad73f20035cc5f934073f9f1
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-16 20:16:14 +0100

    refactor: rewrite mailer subject

--- tests/mailer-subject.vo.test.ts
+++ tests/mailer-subject.vo.test.ts
@@ -1,25 +1,26 @@
 import { describe, expect, test } from "bun:test";
+import * as v from "valibot";
 import { MailerSubject } from "../src/mailer-subject.vo";
 
 describe("MailerSubject", () => {
   test("happy path", () => {
-    expect(MailerSubject.safeParse("a".repeat(128)).success).toEqual(true);
-    expect(MailerSubject.safeParse("A".repeat(128)).success).toEqual(true);
+    expect(v.safeParse(MailerSubject, "a".repeat(128)).success).toEqual(true);
+    expect(v.safeParse(MailerSubject, "A".repeat(128)).success).toEqual(true);
   });
 
   test("rejects non-string - null", () => {
-    expect(() => MailerSubject.parse(null)).toThrow("mailer.subject.invalid");
+    expect(() => v.parse(MailerSubject, null)).toThrow("mailer.subject.invalid");
   });
 
   test("rejects non-string - number", () => {
-    expect(() => MailerSubject.parse(123)).toThrow("mailer.subject.invalid");
+    expect(() => v.parse(MailerSubject, 123)).toThrow("mailer.subject.invalid");
   });
 
   test("rejects empty", () => {
-    expect(() => MailerSubject.parse("")).toThrow("mailer.subject.invalid");
+    expect(() => v.parse(MailerSubject, "")).toThrow("mailer.subject.invalid");
   });
 
   test("rejects too long", () => {
-    expect(() => MailerSubject.parse(`${"a".repeat(128)}a`)).toThrow("mailer.subject.invalid");
+    expect(() => v.parse(MailerSubject, `${"a".repeat(128)}a`)).toThrow("mailer.subject.invalid");
   });
 });
