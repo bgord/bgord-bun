505e75de7e0ca23064e587e1d533b41fd95ce9b6
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-20 18:29:30 +0100

    step: add alert channel with logger

---
+++ src/alert-channel-with-logger.adapter.ts
@@ -0,0 +1,46 @@
+import type { AlertChannelPort } from "./alert-channel.port";
+import type { AlertMessage } from "./alert-message.vo";
+import type { ClockPort } from "./clock.port";
+import type { LoggerPort } from "./logger.port";
+import { Stopwatch } from "./stopwatch.service";
+
+export type AlertChannelWithLoggerAdapterDependencies = {
+  inner: AlertChannelPort;
+  Logger: LoggerPort;
+  Clock: ClockPort;
+};
+
+export class AlertChannelWithLoggerAdapter implements AlertChannelPort {
+  private readonly base = { component: "infra", operation: "alert_channel" };
+
+  constructor(private readonly deps: AlertChannelWithLoggerAdapterDependencies) {}
+
+  async send(alert: AlertMessage): Promise<void> {
+    const duration = new Stopwatch(this.deps);
+
+    try {
+      this.deps.Logger.info({ message: "AlertChannel attempt", metadata: alert.toJSON(), ...this.base });
+
+      await this.deps.inner.send(alert);
+
+      this.deps.Logger.info({
+        message: "AlertChannel success",
+        metadata: { alert: alert.toJSON(), duration: duration.stop() },
+        ...this.base,
+      });
+    } catch (error) {
+      this.deps.Logger.error({
+        message: "AlertChannel error",
+        error,
+        metadata: duration.stop(),
+        ...this.base,
+      });
+
+      throw error;
+    }
+  }
+
+  async verify(): Promise<boolean> {
+    return this.deps.inner.verify();
+  }
+}
---
+++ tests/alert-channel-with-logger.adapter.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, spyOn, test } from "bun:test";
+import * as tools from "@bgord/tools";
+import { AlertChannelNoopAdapter } from "../src/alert-channel-noop.adapter";
+import { AlertChannelWithLoggerAdapter } from "../src/alert-channel-with-logger.adapter";
+import { ClockFixedAdapter } from "../src/clock-fixed.adapter";
+import { LoggerCollectingAdapter } from "../src/logger-collecting.adapter";
+import * as mocks from "./mocks";
+
+const Clock = new ClockFixedAdapter(mocks.TIME_ZERO);
+
+const inner = new AlertChannelNoopAdapter();
+
+describe("AlertChannelWithLoggerAdapter", () => {
+  test("send - success", async () => {
+    const Logger = new LoggerCollectingAdapter();
+    const adapter = new AlertChannelWithLoggerAdapter({ inner, Logger, Clock });
+
+    await adapter.send(mocks.alert);
+
+    expect(Logger.entries).toEqual([
+      {
+        component: "infra",
+        operation: "alert_channel",
+        message: "AlertChannel attempt",
+        metadata: mocks.alert.toJSON(),
+      },
+      {
+        component: "infra",
+        operation: "alert_channel",
+        message: "AlertChannel success",
+        metadata: { alert: mocks.alert.toJSON(), duration: expect.any(tools.Duration) },
+      },
+    ]);
+  });
+
+  test("send - failure", async () => {
+    using _ = spyOn(inner, "send").mockImplementation(mocks.throwIntentionalErrorAsync);
+    const Logger = new LoggerCollectingAdapter();
+    const adapter = new AlertChannelWithLoggerAdapter({ inner, Logger, Clock });
+
+    expect(async () => adapter.send(mocks.alert)).toThrow(mocks.IntentionalError);
+
+    expect(Logger.entries[0]).toEqual({
+      component: "infra",
+      operation: "alert_channel",
+      message: "AlertChannel attempt",
+      metadata: mocks.alert.toJSON(),
+    });
+    expect(Logger.entries[1]).toEqual({
+      component: "infra",
+      operation: "alert_channel",
+      message: "AlertChannel error",
+      error: new Error(mocks.IntentionalError),
+      metadata: expect.any(tools.Duration),
+    });
+  });
+
+  test("verify", async () => {
+    const Logger = new LoggerCollectingAdapter();
+    const adapter = new AlertChannelWithLoggerAdapter({ inner, Logger, Clock });
+
+    expect(await adapter.verify()).toEqual(true);
+  });
+});
