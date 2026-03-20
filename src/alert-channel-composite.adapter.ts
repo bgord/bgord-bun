72e94f02938f9a8934c759b1ef4ac64569ffa131
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-20 18:45:42 +0100

    step: add channels min max

--- src/alert-channel-composite.adapter.ts
+++ src/alert-channel-composite.adapter.ts
@@ -1,8 +1,16 @@
 import type { AlertChannelPort } from "./alert-channel.port";
 import type { AlertMessage } from "./alert-message.vo";
 
+export const AlertChannelCompositeError = {
+  Min: "alert.channel.composite.channels.min",
+  Max: "alert.channel.composite.channels.max",
+};
+
 export class AlertChannelCompositeAdapter implements AlertChannelPort {
-  constructor(private readonly channels: ReadonlyArray<AlertChannelPort>) {}
+  constructor(private readonly channels: ReadonlyArray<AlertChannelPort>) {
+    if (this.channels.length === 0) throw new Error(AlertChannelCompositeError.Min);
+    if (this.channels.length > 5) throw new Error(AlertChannelCompositeError.Max);
+  }
 
   async send(alert: AlertMessage): Promise<void> {
     await Promise.allSettled(this.channels.map((channel) => channel.send(alert)));
