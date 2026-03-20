c193ab8050c166a103bd0d0cddab918912d7d691
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-20 17:17:56 +0100

    step: add sms with timeout adapter

---
+++ src/sms-with-timeout.adapter.ts
@@ -0,0 +1,22 @@
+import type * as tools from "@bgord/tools";
+import type { SmsPort } from "./sms.port";
+import type { SmsMessage } from "./sms-message.vo";
+import type { TimeoutRunnerPort } from "./timeout-runner.port";
+
+export type SmsWithTimeoutAdapterDependencies = { TimeoutRunner: TimeoutRunnerPort; inner: SmsPort };
+export type SmsWithTimeoutAdapterConfig = { timeout: tools.Duration };
+
+export class SmsWithTimeoutAdapter implements SmsPort {
+  constructor(
+    private readonly config: SmsWithTimeoutAdapterConfig,
+    private readonly deps: SmsWithTimeoutAdapterDependencies,
+  ) {}
+
+  async send(message: SmsMessage): Promise<void> {
+    await this.deps.TimeoutRunner.run(this.deps.inner.send(message), this.config.timeout);
+  }
+
+  async verify(): Promise<boolean> {
+    return this.deps.inner.verify();
+  }
+}
