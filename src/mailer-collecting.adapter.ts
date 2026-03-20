8b0246beee6e5ee5a8f5b701c8da3b0571c7a56d
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-20 18:19:22 +0100

    step: add mailer collecting

---
+++ src/mailer-collecting.adapter.ts
@@ -0,0 +1,14 @@
+import type { MailerPort } from "./mailer.port";
+import type { MailerTemplate } from "./mailer-template.vo";
+
+export class MailerCollectingAdapter implements MailerPort {
+  messages: MailerTemplate[] = [];
+
+  async send(template: MailerTemplate): Promise<void> {
+    this.messages.push(template);
+  }
+
+  async verify(): Promise<boolean> {
+    return true;
+  }
+}
