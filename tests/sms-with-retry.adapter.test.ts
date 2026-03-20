ee8ac672dd97a6210989957e5f9c8abddadfddc2
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-20 17:16:20 +0100

    step: add sms with retry adapter

--- tests/sms-with-retry.adapter.test.ts
+++ tests/sms-with-retry.adapter.test.ts
@@ -1,30 +1,37 @@
 import { describe, expect, spyOn, test } from "bun:test";
 import * as tools from "@bgord/tools";
-import { MailerNoopAdapter } from "../src/mailer-noop.adapter";
-import { MailerWithRetryAdapter } from "../src/mailer-with-retry.adapter";
+import * as v from "valibot";
 import { RetryBackoffNoopStrategy } from "../src/retry-backoff-noop.strategy";
 import { SleeperNoopAdapter } from "../src/sleeper-noop.adapter";
+import { SmsBody } from "../src/sms-body.vo";
+import { SmsMessage } from "../src/sms-message.vo";
+import { SmsNoopAdapter } from "../src/sms-noop.adapter";
+import { SmsWithRetryAdapter } from "../src/sms-with-retry.adapter";
+import { TelephoneNumber } from "../src/telephone-number.vo";
 import * as mocks from "./mocks";
+
+const to = v.parse(TelephoneNumber, "+12125551234");
+const body = v.parse(SmsBody, "Your OTP is 123456");
+const message = new SmsMessage(to, body);
 
 const max = tools.Int.positive(3);
 const backoff = new RetryBackoffNoopStrategy();
 const retry = { max, backoff };
 
 const Sleeper = new SleeperNoopAdapter();
+const inner = new SmsNoopAdapter();
+const adapter = new SmsWithRetryAdapter({ retry }, { inner, Sleeper });
 
-const inner = new MailerNoopAdapter();
-const adapter = new MailerWithRetryAdapter({ retry }, { inner, Sleeper });
-
-describe("MailerWithRetryAdapter", () => {
+describe("SmsWithRetryAdapter", () => {
   test("send - success", async () => {
-    expect(async () => adapter.send(mocks.template)).not.toThrow();
+    expect(async () => adapter.send(message)).not.toThrow();
   });
 
   test("send - retry", async () => {
     using sleeperWait = spyOn(Sleeper, "wait");
     using _ = spyOn(inner, "send").mockImplementation(mocks.throwIntentionalError);
 
-    expect(async () => adapter.send(mocks.template)).toThrow(mocks.IntentionalError);
+    expect(async () => adapter.send(message)).toThrow(mocks.IntentionalError);
     expect(sleeperWait).toHaveBeenCalledTimes(2);
   });
 
@@ -32,7 +39,7 @@ describe("MailerWithRetryAdapter", () => {
     using sleeperWait = spyOn(Sleeper, "wait");
     using _ = spyOn(inner, "send").mockImplementationOnce(mocks.throwIntentionalError);
 
-    expect(async () => adapter.send(mocks.template)).not.toThrow();
+    expect(async () => adapter.send(message)).not.toThrow();
     expect(sleeperWait).toHaveBeenCalledTimes(1);
   });
 
