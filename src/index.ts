d24438232fb40430e1644d326c3586344cf5f8ef
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-30 22:28:05 +0200

    step: add job failer port

--- src/index.ts
+++ src/index.ts
@@ -243,6 +243,7 @@ export * from "./job-enqueuer.port";
 export * from "./job-enqueuer-collecting.adapter";
 export * from "./job-enqueuer-noop.adapter";
 export * from "./job-envelope";
+export * from "./job-failer.port";
 export * from "./language-detector.middleware";
 export * from "./language-detector.strategy";
 export * from "./language-detector-cookie.strategy";
