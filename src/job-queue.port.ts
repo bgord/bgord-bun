8a91daccd3230bd955387db97853f2b0367194f0
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-31 00:25:31 +0200

    step: add job queue port

---
+++ src/job-queue.port.ts
@@ -0,0 +1,12 @@
+import type * as tools from "@bgord/tools";
+import type { GenericJob } from "./job.types";
+
+export interface JobQueuePort<Job extends GenericJob> {
+  enqueue<EnqueuedJob extends Job>(job: EnqueuedJob): Promise<EnqueuedJob>;
+
+  claim(limit?: tools.IntegerPositiveType): Promise<ReadonlyArray<Job>>;
+
+  complete(id: GenericJob["id"]): Promise<void>;
+
+  fail(id: GenericJob["id"]): Promise<void>;
+}
