22c13a557b25ab37b85573fabac7720056c9f78e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-31 17:35:50 +0200

    step: add job completer sqlite

---
+++ src/job-completer-sqlite.adapter.ts
@@ -0,0 +1,13 @@
+import type { Database } from "bun:sqlite";
+import type { GenericJob } from "./job.types";
+import type { JobCompleterPort } from "./job-completer.port";
+
+type Dependencies = { db: Database };
+
+export class JobCompleterSqliteAdapter implements JobCompleterPort {
+  constructor(private readonly deps: Dependencies) {}
+
+  async complete(id: GenericJob["id"]): Promise<void> {
+    this.deps.db.run("UPDATE jobs SET status = 'completed' WHERE id = ?", [id]);
+  }
+}
