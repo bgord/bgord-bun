8a52459745af684b9766b280e2b895d17e84942b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-31 14:07:10 +0200

    refactor: improve formatting

--- src/job-requeuer-collecting.adapter.ts
+++ src/job-requeuer-collecting.adapter.ts
@@ -2,11 +2,7 @@ import type * as tools from "@bgord/tools";
 import type { GenericJob } from "./job.types";
 import type { JobRequeuerPort } from "./job-requeuer.port";
 
-type RequeueEntry = {
-  id: GenericJob["id"];
-  revision: GenericJob["revision"];
-  delay: tools.Duration;
-};
+type RequeueEntry = { id: GenericJob["id"]; revision: GenericJob["revision"]; delay: tools.Duration };
 
 export class JobRequeuerCollectingAdapter implements JobRequeuerPort {
   readonly requeued: Array<RequeueEntry> = [];
