6c84d2aec785bb9effe11eba2ca3046db60f2328
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-31 15:18:46 +0200

    step: update packages

--- tests/job-requeuer-collecting.adapter.test.ts
+++ tests/job-requeuer-collecting.adapter.test.ts
@@ -3,12 +3,11 @@ import * as tools from "@bgord/tools";
 import { JobRequeuerCollectingAdapter } from "../src/job-requeuer-collecting.adapter";
 import * as mocks from "./mocks";
 
+const delay = tools.Duration.Seconds(5);
 const requeuer = new JobRequeuerCollectingAdapter();
 
 describe("JobRequeuerCollectingAdapter", () => {
   test("requeue", async () => {
-    const delay = tools.Duration.Seconds(5);
-
     await requeuer.requeue(mocks.correlationId, 1, delay);
 
     expect(requeuer.requeued).toEqual([{ id: mocks.correlationId, revision: 1, delay }]);
