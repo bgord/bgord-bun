5bb84f815b1794314a0a7add70e215a17b7a2ddd
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-01 23:14:56 +0200

    refactor: improve retry

--- src/retry-backoff-noop.strategy.ts
+++ src/retry-backoff-noop.strategy.ts
@@ -2,7 +2,7 @@ import * as tools from "@bgord/tools";
 import type { RetryBackoffStrategy } from "./retry-backoff.strategy";
 
 export class RetryBackoffNoopStrategy implements RetryBackoffStrategy {
-  next(): tools.Duration {
+  next(_attempt: tools.IntegerPositiveType): tools.Duration {
     return tools.Duration.Ms(0);
   }
 }
--- tests/retry-backoff-noop.strategy.test.ts
+++ tests/retry-backoff-noop.strategy.test.ts
@@ -3,9 +3,10 @@ import * as tools from "@bgord/tools";
 import { RetryBackoffNoopStrategy } from "../src/retry-backoff-noop.strategy";
 
 const backoff = new RetryBackoffNoopStrategy();
+const attempt = tools.Int.positive(1);
 
 describe("RetryBackoffNoopStrategy", () => {
   test("next", () => {
-    expect(backoff.next().equals(tools.Duration.Ms(0))).toEqual(true);
+    expect(backoff.next(attempt).equals(tools.Duration.Ms(0))).toEqual(true);
   });
 });
