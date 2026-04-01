692979e541da977182409f9949d78239061fc1f9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-01 14:30:41 +0200

    refactor: improve directory ensurer

--- src/directory-ensurer.adapter.ts
+++ src/directory-ensurer.adapter.ts
@@ -1,9 +1,8 @@
 import fs from "node:fs/promises";
-import type * as tools from "@bgord/tools";
-import type { DirectoryEnsurerPort } from "./directory-ensurer.port";
+import type { DirectoryEnsurerConfig, DirectoryEnsurerPort } from "./directory-ensurer.port";
 
 export class DirectoryEnsurerAdapter implements DirectoryEnsurerPort {
-  async ensure(path: tools.DirectoryPathAbsoluteType | tools.DirectoryPathRelativeType): Promise<void> {
-    await fs.mkdir(path, { recursive: true });
+  async ensure(config: DirectoryEnsurerConfig): Promise<void> {
+    await fs.mkdir(config, { recursive: true });
   }
 }
