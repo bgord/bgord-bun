5ee9814f1922556cad19a3d2eea1223f4e29e80c
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-01 16:18:29 +0200

    refactor: improve cache repo noop

--- src/cache-repository-noop.adapter.ts
+++ src/cache-repository-noop.adapter.ts
@@ -1,13 +1,11 @@
 import type { CacheRepositoryPort } from "./cache-repository.port";
+import type { Hash } from "./hash.vo";
 
 export class CacheRepositoryNoopAdapter implements CacheRepositoryPort {
-  async get<T>(): Promise<T | null> {
+  async get<T>(_subject: Hash): Promise<T | null> {
     return null;
   }
-
-  async set(): Promise<void> {}
-
-  async delete(): Promise<void> {}
-
+  async set<T>(_subject: Hash, _value: T): Promise<void> {}
+  async delete(_subject: Hash): Promise<void> {}
   async flush(): Promise<void> {}
 }
