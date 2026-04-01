1995a03cc14cb24da7d6f20f3c8e4cfadca197d3
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-01 23:17:13 +0200

    refactor: improve timeout

--- src/timeout-cancellable-runner-noop.adapter.ts
+++ src/timeout-cancellable-runner-noop.adapter.ts
@@ -1,7 +1,8 @@
+import type * as tools from "@bgord/tools";
 import type { TimeoutCancellableRunnerPort } from "./timeout-cancellable-runner.port";
 
 export class TimeoutCancellableRunnerNoop implements TimeoutCancellableRunnerPort {
-  async cancellable<T>(action: (signal: AbortSignal) => Promise<T>): Promise<T> {
+  async cancellable<T>(action: (signal: AbortSignal) => Promise<T>, _timeout: tools.Duration): Promise<T> {
     const controller = new AbortController();
 
     return action(controller.signal);
