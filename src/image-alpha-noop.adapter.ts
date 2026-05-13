158c03ede3b8232ab0e062d45cfa70ed691acbac
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-13 20:32:25 +0200

    step: remove image alpha

--- src/image-alpha-noop.adapter.ts
+++
@@ -1,8 +0,0 @@
-import type * as tools from "@bgord/tools";
-import type { ImageAlphaPort, ImageAlphaStrategy } from "./image-alpha.port";
-
-export class ImageAlphaNoopAdapter implements ImageAlphaPort {
-  async flatten(recipe: ImageAlphaStrategy): Promise<tools.FilePathRelative | tools.FilePathAbsolute> {
-    return recipe.strategy === "output_path" ? recipe.output : recipe.input;
-  }
-}
