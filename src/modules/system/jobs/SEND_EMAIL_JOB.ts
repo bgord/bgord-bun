f67e6a7d8d1af1304b3c811847d9246f3fa99725
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-11 20:12:04 +0200

    Fix send email job cycle

--- src/modules/system/jobs/SEND_EMAIL_JOB.ts
+++ src/modules/system/jobs/SEND_EMAIL_JOB.ts
@@ -1,19 +1,21 @@
-import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
+import { JobEnvelopeSchema } from "../../../job-envelope";
+import { MailerContentHtml } from "../../../mailer-content-html.vo";
+import { MailerSubject } from "../../../mailer-subject.vo";
 
 // Stryker disable all
 export const SEND_EMAIL_JOB = "SEND_EMAIL_JOB";
 // Stryker restore all
 
 export const SendEmailJobSchema = v.object({
-  ...bg.JobEnvelopeSchema,
+  ...JobEnvelopeSchema,
   name: v.literal(SEND_EMAIL_JOB),
   payload: v.object({
     from: tools.Email,
     to: tools.Email,
-    subject: bg.MailerSubject,
-    html: bg.MailerContentHtml,
+    subject: MailerSubject,
+    html: MailerContentHtml,
   }),
 });
 
