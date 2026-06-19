2ac59fa8b5815b7d727107040a97f47e94742720
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-19 13:11:09 +0200

    Define translations provider methods

--- src/translations-provider.port.ts
+++ src/translations-provider.port.ts
@@ -1,1 +1,12 @@
-export type TranslationsProviderPort = {};
+import type * as tools from "@bgord/tools";
+
+type TranslationsKeyType = string;
+type TranslationsValueType = string;
+type TranslationsType = Record<TranslationsKeyType, TranslationsValueType>;
+
+// TODO: getters
+export interface TranslationsProviderPort {
+  getTranslationsFor(language: tools.LanguageType): Promise<TranslationsType | null>;
+
+  getLanguages(): ReadonlyArray<tools.LanguageType>;
+}
