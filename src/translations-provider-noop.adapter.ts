a5f6963e8e571cc41af3cb940f48445156a8546a
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-19 13:14:42 +0200

    Add translations provider noop adapter

---
+++ src/translations-provider-noop.adapter.ts
@@ -0,0 +1,17 @@
+import type * as tools from "@bgord/tools";
+import type { TranslationsProviderPort, TranslationsType } from "./translations-provider.port";
+
+export class TranslationsProviderNoopAdapter implements TranslationsProviderPort {
+  constructor(
+    private readonly languages: ReadonlyArray<tools.LanguageType>,
+    private readonly translations: Record<tools.LanguageType, TranslationsType>,
+  ) {}
+
+  async getTranslationsFor(language: tools.LanguageType): Promise<TranslationsType | null> {
+    return this.translations[language] ?? null;
+  }
+
+  getLanguages(): ReadonlyArray<tools.LanguageType> {
+    return this.languages;
+  }
+}
