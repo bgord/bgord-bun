5887fd7bfde2428acc73fc244357fc36d337feac
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-19 13:14:42 +0200

    Add translations provider noop adapter

--- src/translations-provider.port.ts
+++ src/translations-provider.port.ts
@@ -2,7 +2,7 @@ import type * as tools from "@bgord/tools";
 
 type TranslationsKeyType = string;
 type TranslationsValueType = string;
-type TranslationsType = Record<TranslationsKeyType, TranslationsValueType>;
+export type TranslationsType = Record<TranslationsKeyType, TranslationsValueType>;
 
 // TODO: getters
 export interface TranslationsProviderPort {
