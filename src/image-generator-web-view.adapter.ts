86f31af445599b506d0d5a7ad7a567880335dadc
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-23 14:46:18 +0200

    Bump bgord/scripts

--- src/image-generator-web-view.adapter.ts
+++ src/image-generator-web-view.adapter.ts
@@ -8,6 +8,7 @@ export class ImageGeneratorWebViewAdapter implements ImageGeneratorPort {
   async generate(config: ImageGeneratorConfig): Promise<Uint8Array<ArrayBuffer>> {
     await using view = new Bun.WebView(config);
 
+    // TODO: Potential XSS risk
     await view.navigate(`data:text/html;charset=utf-8,${encodeURIComponent(config.template)}`);
 
     const image = await view.screenshot({
