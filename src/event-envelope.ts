46e6e9b84c12f7b2ebf70c35233c8a8b1ed494aa
Bartosz Gordon <gordonbartosz@gmail.com>
2026-07-28 13:01:13 +0200

    Add commit field to the event envelope

--- src/event-envelope.ts
+++ src/event-envelope.ts
@@ -1,12 +1,19 @@
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
+import type { BuildInfoType } from "./build-info.vo";
 import type { ClockPort } from "./clock.port";
+import { CommitShaValue } from "./commit-sha-value.vo";
 import { CorrelationStorage } from "./correlation-storage.service";
 import { EventStream, type EventStreamType } from "./event-stream.vo";
 import type { IdProviderPort } from "./id-provider.port";
+import type { ReactiveConfigPort } from "./reactive-config.port";
 import { UUID } from "./uuid.vo";
 
-type Dependencies = { IdProvider: IdProviderPort; Clock: ClockPort };
+type Dependencies = {
+  IdProvider: IdProviderPort;
+  Clock: ClockPort;
+  BuildInfoConfig: ReactiveConfigPort<BuildInfoType>;
+};
 
 export const EventEnvelopeSchema = {
   id: UUID,
@@ -15,24 +22,26 @@ export const EventEnvelopeSchema = {
   stream: EventStream,
   version: v.literal(1),
   revision: v.optional(tools.RevisionValue),
+  commit: CommitShaValue,
 };
 
-const createEventEnvelope = (stream: EventStreamType, deps: Dependencies) =>
+const createEventEnvelope = async (stream: EventStreamType, deps: Dependencies) =>
   ({
     id: deps.IdProvider.generate(),
     correlationId: CorrelationStorage.get(),
     createdAt: deps.Clock.now().ms,
     stream,
+    commit: (await deps.BuildInfoConfig.get()).sha,
   }) as const;
 
-export function event<Schema extends v.ObjectSchema<any, any>>(
+export async function event<Schema extends v.ObjectSchema<any, any>>(
   schema: Schema,
   stream: EventStreamType,
   payload: v.InferOutput<Schema>["payload"],
   deps: Dependencies,
-): v.InferOutput<Schema> {
+): Promise<v.InferOutput<Schema>> {
   return v.parse(schema, {
-    ...createEventEnvelope(stream, deps),
+    ...(await createEventEnvelope(stream, deps)),
     name: schema.entries.name.literal,
     version: schema.entries.version.literal,
     payload,
