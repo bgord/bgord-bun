82b249b66af96a11cbb1d3f22852e36cda5ceff9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-06 08:17:42 +0200

    Drop unused error codes in hcaptcha result

--- src/hcaptcha.service.ts
+++ src/hcaptcha.service.ts
@@ -4,7 +4,7 @@ export const HCaptchaServiceError = { Error: "hcaptcha.service.error" };
 
 type HcaptchaTokenType = string;
 
-export type HCaptchaVerificationResult = { success: boolean; errorCodes?: ReadonlyArray<string> };
+export type HCaptchaVerificationResult = { success: boolean };
 
 export class HCaptchaService {
   async verify(
