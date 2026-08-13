31ece8ea88fe1f36726fd203dd86366f0337f562
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-13 13:53:04 +0200

    Add Smtp port branding

--- src/smtp-port.vo.ts
+++ src/smtp-port.vo.ts
@@ -1,5 +1,5 @@
-import type * as v from "valibot";
+import * as v from "valibot";
 import { Port } from "./port.vo";
 
-export const SmtpPort = Port;
+export const SmtpPort = v.pipe(Port, v.brand("SmtpPort"));
 export type SmtpPortType = v.InferOutput<typeof SmtpPort>;
