c2d1466f0fb47f4703d515c64300777bf6beb6fe
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-19 14:49:24 +0200

    Use codec in the reactive config with cache adapter

--- src/reactive-config-with-cache.adapter.ts
+++ src/reactive-config-with-cache.adapter.ts
@@ -1,10 +1,16 @@
+import { CacheCodecIdentityStrategy } from "./cache-codec-identity.strategy";
 import type { CacheResolverStrategy } from "./cache-resolver.strategy";
+import type { CacheValueType } from "./cache-value.vo";
 import type { HashContentStrategy } from "./hash-content.strategy";
 import type { ReactiveConfigPort } from "./reactive-config.port";
 
 type Dependencies = { CacheResolver: CacheResolverStrategy; HashContent: HashContentStrategy };
 
-export class ReactiveConfigWithCacheAdapter<T extends object> implements ReactiveConfigPort<T> {
+export class ReactiveConfigWithCacheAdapter<T extends object & CacheValueType>
+  implements ReactiveConfigPort<T>
+{
+  private readonly codec = new CacheCodecIdentityStrategy<T>();
+
   constructor(
     private readonly inner: ReactiveConfigPort<T>,
     private readonly subject: string,
@@ -14,6 +20,6 @@ export class ReactiveConfigWithCacheAdapter<T extends object> implements Reactiv
   async get(): Promise<Readonly<T>> {
     const key = await this.deps.HashContent.hash(this.subject);
 
-    return Object.freeze(await this.deps.CacheResolver.resolve(key, () => this.inner.get()));
+    return Object.freeze(await this.deps.CacheResolver.resolve<T>(key, () => this.inner.get(), this.codec));
   }
 }
