42f08df5ef1795976e3af1a8ab6b7dd5d4509d56
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-19 14:44:05 +0200

    Use codec in the api version middleware

--- src/api-version.middleware.ts
+++ src/api-version.middleware.ts
@@ -1,5 +1,6 @@
 import type * as tools from "@bgord/tools";
 import type { BuildInfoType } from "./build-info.vo";
+import { CacheCodecIdentityStrategy } from "./cache-codec-identity.strategy";
 import type { CacheResolverStrategy } from "./cache-resolver.strategy";
 import type { HashContentStrategy } from "./hash-content.strategy";
 import type { ReactiveConfigPort } from "./reactive-config.port";
@@ -16,6 +17,7 @@ export class ApiVersionMiddleware {
   static readonly HEADER_NAME = "api-version";
 
   private readonly resolver: SubjectApplicationResolver;
+  private readonly codec = new CacheCodecIdentityStrategy<BuildInfoType>();
 
   constructor(private readonly deps: ApiVersionMiddlewareDependencies) {
     this.resolver = new SubjectApplicationResolver(
@@ -27,8 +29,10 @@ export class ApiVersionMiddleware {
   async evaluate(): Promise<tools.PackageVersionSchemaType> {
     const subject = await this.resolver.resolve();
 
-    const build = await this.deps.CacheResolver.resolve(subject.hex, async () =>
-      this.deps.BuildInfoConfig.get(),
+    const build = await this.deps.CacheResolver.resolve<BuildInfoType>(
+      subject.hex,
+      async () => this.deps.BuildInfoConfig.get(),
+      this.codec,
     );
 
     return build.version;
