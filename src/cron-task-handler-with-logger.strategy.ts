c71eb282f372edefc1ee0824763000e25f38a857
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-28 15:25:52 +0200

    Make cron task handler with logger throw

--- src/cron-task-handler-with-logger.strategy.ts
+++ src/cron-task-handler-with-logger.strategy.ts
@@ -35,7 +35,7 @@ export class CronTaskHandlerWithLoggerStrategy implements CronTaskHandlerStrateg
             ...this.base,
           });
 
-          return;
+          throw error;
         }
 
         this.deps.Logger.info({
