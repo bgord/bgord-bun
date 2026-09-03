f4394e2939670d84fe4cb55960d94ce390b0259f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-03 22:10:30 +0200

    Add commit field to GenericEvent type

--- src/event.types.ts
+++ src/event.types.ts
@@ -1,4 +1,5 @@
 import type * as tools from "@bgord/tools";
+import type { CommitShaValueType } from "./commit-sha-value.vo";
 import type { CorrelationIdType } from "./correlation-id.vo";
 import type { EventStreamType } from "./event-stream.vo";
 import type { UUIDType } from "./uuid.vo";
@@ -9,6 +10,7 @@ export type GenericEvent = {
   createdAt: number;
   stream: EventStreamType;
   revision?: tools.RevisionValueType;
+  commit: CommitShaValueType;
   name: string;
   version: number;
   payload: Record<string, unknown>;
